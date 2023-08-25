5c7377b63218a8b9d290fd46a924f5c584c84f0b
Diandre Miller <millerdiandre@gmail.com>
2023-08-25 10:34:25 -0400

    added customer testimonials

---
+++ src/NonMVPComponents/CustomerTestimonialsComponent.jsx
@@ -0,0 +1,13 @@
+//This component will be for customer testimonials
+
+const CustomerTestimonialsComponent = () => {
+    return (
+        <div>
+            <h3>
+                This is the customer testimonal component
+            </h3>
+        </div>
+    )
+};
+
+export default CustomerTestimonialsComponent;
