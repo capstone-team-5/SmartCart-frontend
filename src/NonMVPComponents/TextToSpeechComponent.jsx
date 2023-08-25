ebde81c72ffed59937bf200e97916c71e714c36b
Diandre Miller <millerdiandre@gmail.com>
2023-08-25 10:50:09 -0400

    added text to speech and ideas and questions files

---
+++ src/NonMVPComponents/TextToSpeechComponent.jsx
@@ -0,0 +1,11 @@
+const TextToSpeechComponent = () => {
+    return (
+        <div>
+            <h3>
+                This is the text to speech component
+            </h3>
+        </div>
+    )
+};
+
+export default TextToSpeechComponent;
