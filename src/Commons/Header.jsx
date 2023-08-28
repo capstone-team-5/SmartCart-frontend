6091dd06e7e6dc31c946a587844ad88760a869b5
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-08-28 15:41:55 -0400

    add tailwind to header for testingg

--- src/Commons/Header.jsx
+++ src/Commons/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-bold underline">This is the header</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">This is the header</h2>
+    </div>
+  );
 };
 
 export default Header;
