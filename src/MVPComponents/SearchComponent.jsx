b499bfd410302903e41f56dc32ae3f506746dcb4
Diandre Miller <millerdiandre@gmail.com>
2023-08-27 20:20:17 -0400

    search functionality

--- src/MVPComponents/SearchComponent.jsx
+++ src/MVPComponents/SearchComponent.jsx
@@ -1,13 +1,62 @@
-//This function will allow users to search for grocery items
+// This function will allow users to search for grocery items
 
-const SearchComponent = () => {
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const SearchComponent = ({addToCart}) => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [products, setProducts] = useState([]);
+    const [clickedProduct, setClickedProduct] = useState(false);
+
+    useEffect(() => {
+        if (searchQuery) {
+            axios.get(`${process.env.REACT_APP_BACKEND_API}/products`)
+                .then((response) => {
+                    console.log(response.data)
+                    const items = response.data;
+                    const foundItems = items.filter((item) => 
+                        item.product_name.toLowerCase().includes(searchQuery.toLowerCase())
+                    );
+                    setProducts(foundItems);
+                })
+                .catch((error) => console.log(error))
+        } else {
+            setProducts([]);
+        }
+        setClickedProduct(false);
+    }, [searchQuery])
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+        setClickedProduct(false);
+    };
+
+    const handleProductClicked = () => {
+        setSearchQuery('');
+        setClickedProduct([]);
+        setClickedProduct(true);
+    }
+    
     return (
         <div>
-            <h3>
-                This is the search component
-            </h3>
+            <input type='search' placeholder='Search for a product...' value={searchQuery} onChange={handleSearchChange} />
+            {products.length > 0 ? (
+                <div>
+                    {products.map((product) => (
+                        <div key={product.product_id}>
+                            <Link to={`/products/${product.product_id}`} onClick={handleProductClicked}>
+                                <h3>{product.product_name}</h3>
+                                {!clickedProduct && <img src={product.product_image} alt={product.product_name} />}
+                            </Link>
+                            <button onClick={addToCart}>Add To Cart</button>
+                        </div>
+                    ))}
+                </div>
+            ) : <div>{searchQuery && !clickedProduct ? 'This product does not exist.' : null}</div>}
         </div>
-    )
+    );
 };
 
 export default SearchComponent;
