a128ccf1b034f069a2ca5f3de8edde1b1d892b8a
Vandhana Mohan <91850987+Vandhana-Mohan@users.noreply.github.com>
2023-08-28 22:43:34 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-// import logo from './logo.svg';
-import "./App.css";
 
 //Dependencies
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-=======
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
->>>>>>> main
 
 //Commons
 import Header from "./Commons/Header";
@@ -18,26 +11,6 @@ import Navbar from "./Commons/Navbar";
 import Footer from "./Commons/Footer";
 
 //Pages
-<<<<<<< HEAD
-import AboutUs from './Pages/AboutUs';
-import Cart from './Pages/Cart';
-import Home from './Pages/Home';
-import Location from './Pages/Location';
-import Login from './Pages/Login';
-import FourOFour from './Pages/FourOFour';
-import SignUp from './Pages/SignUp';
-import User from './Pages/User';
-import UserCart from './Pages/UserCart';
-import UserEdit from './Pages/UserEdit';
-import Subscription from './Pages/Subscription';
-import ConfirmSubscription from './Pages/ConfirmSubscription';
-import MeetTheDevelopers from './Pages/MeetTheDevelopers';
-import TestComponent from './MVPComponents/TestComponent';
-
-//Component
-
-import FilterButtonsComponent from './MVPComponents/FilterButtonsComponent';
-=======
 import AboutUs from "./Pages/AboutUs";
 import Cart from "./Pages/Cart";
 import Home from "./Pages/Home";
@@ -52,36 +25,13 @@ import Subscription from "./Pages/Subscription";
 import ConfirmSubscription from "./Pages/ConfirmSubscription";
 import MeetTheDevelopers from "./Pages/MeetTheDevelopers";
 import TestComponent from "./MVPComponents/TestComponent";
->>>>>>> main
 
 //Component
 
 import FilterButtonsComponent from "./MVPComponents/FilterButtonsComponent";
 
 function App() {
-<<<<<<< HEAD
 
-  const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND_API}/products`)
-    .then((response) => setCart(response.data))
-  },[])
-
-  const handleAddToCart = (food) => {
-    setCart([...cart, { name: food.product_name, image: food.product_image, price: food.product_price, id: food.product_id }],)
-  };
-
-  const handleDeleteItem = (id) => {
-    setCart(cart.filter((food) => food.product_id !== id))
-  };
-
-  const handleClearCart = () => {
-    setCart([])
-  };
-
-
-=======
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
@@ -110,20 +60,13 @@ function App() {
     setCart([]);
   };
 
->>>>>>> main
+
   return (
     <div className="App">
       <BrowserRouter>
-<<<<<<< HEAD
-        
-=======
->>>>>>> main
         <Header addToCart={handleAddToCart} />
         <Navbar />
-
         <Routes>
-<<<<<<< HEAD
-
           <Route element={<Home />} path='/' />
           <Route element={<TestComponent cart={cart} />} path='/test' />
           <Route element={<AboutUs />} path='/about-us' />
@@ -139,39 +82,7 @@ function App() {
           <Route element={<ConfirmSubscription />} path='/user/:id/subscription/confirmed' />
           <Route element={<MeetTheDevelopers />} path='/meet-the-developers' />
           <Route element={<FourOFour />} path='/*' />
-
-=======
-          <Route element={<Home />} path="/" />
-          <Route element={<TestComponent cart={cart} />} path="/test" />
-          <Route element={<AboutUs />} path="/about-us" />
-          <Route element={<FilterButtonsComponent />} path="/filter/:theme" />
-          <Route
-            element={
-              <Cart
-                addToCart={handleAddToCart}
-                deleteItem={handleDeleteItem}
-                clearCart={handleClearCart}
-                cart={cart}
-              />
-            }
-            path="/cart"
-          />
-          <Route element={<Location />} path="/location" />
-          <Route element={<Login />} path="/login" />
-          <Route element={<SignUp />} path="/sign-up" />
-          <Route element={<User />} path="/user:id" />
-          <Route element={<UserCart />} path="/user/:id/cart" />
-          <Route element={<UserEdit />} path="/user/:id/edit" />
-          <Route element={<Subscription />} path="/user/:id/subscription" />
-          <Route
-            element={<ConfirmSubscription />}
-            path="/user/:id/subscription/confirmed"
-          />
-          <Route element={<MeetTheDevelopers />} path="/meet-the-developers" />
-          <Route element={<FourOFour />} path="/*" />
->>>>>>> main
         </Routes>
-
         <Footer />
       </BrowserRouter>
     </div>
