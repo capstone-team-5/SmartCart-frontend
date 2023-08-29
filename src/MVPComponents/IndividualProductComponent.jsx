34a866fda7e8b7093f77538e3c61a53cdc69b39e
Diandre Miller <millerdiandre@gmail.com>
2023-08-28 22:11:09 -0400

    individual products component

---
+++ src/MVPComponents/IndividualProductComponent.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const IndividualProductComponent = () => {
+    return (
+        <div>
+            <h3>This is an individual product</h3>
+        </div>
+    )
+};
+
+export default IndividualProductComponent;
