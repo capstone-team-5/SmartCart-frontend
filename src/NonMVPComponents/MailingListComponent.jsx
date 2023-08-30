06c2e70f7b9e34c2a6d1311752450de92b546927
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-08-30 06:22:17 -0400

    created a fake mailing list form

--- src/NonMVPComponents/MailingListComponent.jsx
+++ src/NonMVPComponents/MailingListComponent.jsx
@@ -1,9 +1,57 @@
 // This component will allow users to receive mailing list information
 
+import React, { useState } from "react";
+
 const MailingListComponent = () => {
+  const [subscribed, setSubscribed] = useState(false);
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleSignUp(event) {
+    event.preventDefault();
+
+    if (!email || !validateEmail(email)) {
+      setErrorMessage("Please enter a valid email");
+    } else {
+      setSubscribed(true);
+      setErrorMessage("");
+    }
+  }
+
+  function validateEmail(email) {
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return pattern.test(email);
+  }
+
   return (
-    <div>
-      <h3>This is the Subscription page</h3>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md p-8 rounded shadow-md">
+        {subscribed ? (
+          <p className="text-center text-blue-600">
+            Thank you! Watch your inbox for deals, new items and more.
+          </p>
+        ) : (
+          <form className="flex flex-col gap-4 sm:flex-row items-center">
+            <label className="text-black text-lg font-bold whitespace-nowrap">
+              Subscribe to get updates!
+            </label>
+            <input
+              type="email"
+              placeholder="✉️ Enter your email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              className="border rounded py-2 px-3 text-black placeholder-black focus:outline-none focus:ring focus:border-blue-300"
+            />
+            {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+            <button
+              onClick={handleSignUp}
+              className="py-2 px-4 bg-blue-500 text-white rounded whitespace-nowrap hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Sign up
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 };
