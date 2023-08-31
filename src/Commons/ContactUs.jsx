c0ef7c689ccdb0ee142e7cf78dea6547fdfba13b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-08-31 16:54:45 -0400

    added contact us component with email api

---
+++ src/Commons/ContactUs.jsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+
+const ContactUs = () => {
+  const form = useRef();
+  const [message, setMessage] = useState("");
+
+  const sendEmail = async (event) => {
+    event.preventDefault();
+
+    try {
+      await emailjs.sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_API_URL
+      );
+      setMessage("Email sent successfully!");
+    } catch (error) {
+      setMessage("An error occurred while sending the email.");
+      console.error(error);
+    }
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" required />
+      <label>Email</label>
+      <input type="email" name="user_email" required />
+      <label>Message</label>
+      <textarea name="message" required />
+      <input type="submit" value="Send" />
+      <div>{message}</div>
+    </form>
+  );
+};
+
+export default ContactUs;
