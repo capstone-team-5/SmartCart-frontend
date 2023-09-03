b2ff3cae19acaf792ae353bb88a4d976f38bb495
touhami-ben <brooklyn.ben76@gmail.com>
2023-09-02 23:27:41 -0400

    backend api

--- src/MVPComponents/HomeComponent.jsx
+++ src/MVPComponents/HomeComponent.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const API = process.env.REACT_APP_API_URL;
+const API = process.env.REACT_APP_BACKEND_API;
 
 const HomeComponent = () => {
   const [products, setProducts] = useState([]);
