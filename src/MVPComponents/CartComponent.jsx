b72295ab8fb8bc588aab6604385b2cd7c1b8ce21
Diandre Miller <millerdiandre@gmail.com>
2023-09-04 18:44:20 -0400

    not yet fixed cart component

--- src/MVPComponents/CartComponent.jsx
+++ src/MVPComponents/CartComponent.jsx
@@ -6,12 +6,12 @@ const CartComponent = ({ addToCart, deleteItem, clearCart, cart }) => {
 
     return (
         <div>
-            <h3>This is the cart component</h3>
-            {cart.map((food) => {
-                return <div key={food.product_id}>
-                    <button onClick={() => addToCart(food.product_name, food.product_image)}>Add to Cart</button>
-                </div>
-            })}
+                {/* <h3>This is the cart component</h3>
+                {cart.map((food) => {
+                    return <div key={food.product_id}>
+                        <button onClick={() => addToCart(food.product_name, food.product_image)}>Add to Cart</button>
+                    </div>
+                })} */}
         </div>
         
     )
