a7fb460c7687ae98f6bb4126e2b64802a6e8565f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-06 02:09:26 -0400

    adjusting the navbar based on different devices

--- src/Commons/Navbar.jsx
+++ src/Commons/Navbar.jsx
@@ -10,25 +10,20 @@ const navLinks = [
     title: "Home",
     link: "/",
   },
-
   {
     title: "About Us",
     link: "/about-us",
   },
   {
-    title: "Contact-Us",
-    link: "/contact-us",
-  },
-  {
-    title: "Meet the Developers",
-    link: "/meet-the-developers",
-  },
-  {
-    title: "Products",
+    title: "View Products",
     link: "/",
   },
   {
-    title: "Feedback",
+    title: "My Favorites ♥️",
+    link: "/",
+  },
+  {
+    title: "Categories",
     link: "/",
   },
 ];
@@ -37,7 +32,6 @@ const Navbar = () => {
 
   const handleMenu = () => {
     setOpen((prev) => !prev);
-    console.log(open);
   };
   return (
     <div className="bg-white">
@@ -48,7 +42,7 @@ const Navbar = () => {
               <img
                 src={logo_image}
                 alt="SmartCART Logo"
-                className="w-24 h-24 lg:w-48 lg:h-48 rounded-full"
+                className="w-20 h-20 lg:w-40 lg:h-40 rounded-full"
               />
             </Link>
           </div>
@@ -56,168 +50,56 @@ const Navbar = () => {
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               {navLinks.map((link, index) => (
-                <a
+                <Link
                   key={index}
-                  className="text-gray-300 transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                  href={link.link}
+                  className="text-black transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  to={link.link}
                 >
                   {link.title}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
           {/* hamburger button */}
           <div className="-mr-2 flex md:hidden">
+            {/* Emojis */}
+            <FiSun className="text-black text-lg mr-2" />
+            <FiMoon className="text-black text-lg mr-2" />
+            <HiOutlineShoppingCart className="text-black text-lg mr-2" />
+            <FaUser className="text-black text-lg mr-2" />
+            {/* Hamburger Icon */}
             <button
               type="button"
               onClick={handleMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-label={open ? "Close Main Menu" : "Open Main Menu"}
+              aria-expanded={open}
             >
-              <span className="sr-only">Open Main Menu </span>
-              {open === true ? <FaTimes /> : <FaBars />}
+              {open ? <FaTimes /> : <FaBars />}
             </button>
           </div>
         </div>
       </div>
       {/* mobile - menu */}
-      {open ? (
+      {open && (
         <div className="md:hidden">
-          <div className="ox-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             {navLinks.map((link, index) => (
-              <a
+              <Link
                 key={index}
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                href={link.link}
+                className="text-black hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                to={link.link}
               >
                 {link.title}
-              </a>
+              </Link>
             ))}
           </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
 export default Navbar;
-
-// const Navbar = () => {
-//   const [showDropdown, setShowDropdown] = useState(false);
-//   const [showUserDropdown, setShowUserDropdown] = useState(false);
-//   const dropdownRef = useRef(null);
-//   const userDropdownRef = useRef(null);
-
-//   const toggleDropdown = () => {
-//     setShowDropdown(!showDropdown);
-//     setShowUserDropdown(false);
-//   };
-
-//   const toggleUserDropdown = () => {
-//     setShowUserDropdown(!showUserDropdown);
-//     setShowDropdown(false);
-//   };
-
-//   // Close dropdowns when clicking outside
-//   useEffect(() => {
-//     const closeDropdowns = (e) => {
-//       if (
-//         dropdownRef.current &&
-//         !dropdownRef.current.contains(e.target) &&
-//         userDropdownRef.current &&
-//         !userDropdownRef.current.contains(e.target)
-//       ) {
-//         setShowDropdown(false);
-//         setShowUserDropdown(false);
-//       }
-//     };
-
-//     document.addEventListener("mousedown", closeDropdowns);
-
-//     return () => {
-//       document.removeEventListener("mousedown", closeDropdowns);
-//     };
-//   }, []);
-
-//   return (
-//     <nav className="bg-white p-4 flex justify-between items-center">
-//       <div className="flex items-center">
-//         <button
-//           onClick={toggleDropdown}
-//           className="text-black mr-4 text-2xl lg:text-3xl"
-//         >
-//           <FaBars />
-//         </button>
-//         <Link to="/">
-//           <img
-//             src={logo_image}
-//             alt="SmartCART Logo"
-//             className="w-24 h-24 lg:w-48 lg:h-48 rounded-full"
-//           />
-//         </Link>
-//       </div>
-//       <div className="flex items-center">
-//         <button className="text-black mx-4 text-xl lg:text-2xl">
-//           <FiSun />
-//           <FiMoon />
-//         </button>
-//         <button className="text-black mx-4 text-xl lg:text-2xl">
-//           <FaHeart />
-//         </button>
-//         <Link to="/cart" className="text-black mx-4 text-xl lg:text-2xl">
-//           <HiOutlineShoppingCart />
-//         </Link>
-//         <div className="relative" ref={userDropdownRef}>
-//           <button
-//             onClick={toggleUserDropdown}
-//             className="text-black text-xl lg:text-2xl"
-//           >
-//             <FaUser />
-//           </button>
-//           {showUserDropdown && (
-//             <div className="absolute top-full right-0 mt-8 bg-white text-gray-800 shadow-lg p-4">
-//               <ul>
-//                 <li style={{ whiteSpace: "nowrap" }}>My Favorites ♥️</li>
-//                 <li style={{ whiteSpace: "nowrap" }}>Profile Settings</li>
-//                 <li>Sign Out</li>
-//               </ul>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//       {showDropdown && (
-//         <div
-//           className="absolute mt-64 top-1/5 left-0 bg-white text-gray-800 shadow-lg p-2"
-//           ref={dropdownRef}
-//         >
-//           <ul>
-//             <Link to="/about-us">
-//               <li>About Us</li>
-//             </Link>
-//             <Link to="/meet-the-developers">
-//               <li>Meet the Developers</li>
-//             </Link>
-//             <Link to="/contact-us">
-//               <li>Contact Us</li>
-//             </Link>
-//             <Link to="/">
-//               <li>Products</li>
-//             </Link>
-//             <Link to="/">
-//               <li>Testimonial</li>
-//             </Link>
-//             <Link to="/">
-//               <li>Feedback</li>
-//             </Link>
-//             <Link to="/">
-//               <li>Connect With Us @ Social Media</li>
-//             </Link>
-//             {/* Add more menu items */}
-//           </ul>
-//         </div>
-//       )}
-//     </nav>
-//   );
-// };
-// export default Navbar;
 
 /* <FaUser className="w-8 h-8 rounded-full" />
     </button>
@@ -229,4 +111,29 @@ export default Navbar;
         <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
         Aprill@gmail.com
         </span>
+
+  {
+    title: "Contact-Us",
+    link: "/contact-us",
+  },
+  {
+    title: "Meet the Developers",
+    link: "/meet-the-developers",
+  },
+  {
+    title: "Feedback",
+    link: "/",
+  },
+  {
+    title: "Review A Store",
+    link: "/",
+  },
+    {
+    title: "Profile Settings",
+    link: "/",
+  },
+  {
+    title: "Sign Out",
+    link: "/",
+  },
 */
