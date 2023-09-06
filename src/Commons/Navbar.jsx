11c025ec8f4a10b7779872c4bf5f52190202503c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-06 04:13:36 -0400

    added cart lenggth to cart icon

--- src/Commons/Navbar.jsx
+++ src/Commons/Navbar.jsx
@@ -26,47 +26,72 @@ const navLinks = [
     title: "Categories",
     link: "/",
   },
+  {
+    title: "Contact-Us",
+    link: "/contact-us",
+  },
+  {
+    title: "Meet the Developers",
+    link: "/meet-the-developers",
+  },
+  {
+    title: "Feedback",
+    link: "/",
+  },
+  {
+    title: "Review A Store",
+    link: "/",
+  },
 ];
 
-const Navbar = () => {
+const Navbar = ({ cartLength }) => {
   const [open, setOpen] = useState(false);
 
   const handleMenu = () => {
     setOpen((prev) => !prev);
   };
-
   return (
     <div className="bg-white">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          {/* hamburger button */}
-          <div className="flex items-center md:hidden">
+        <div className="flex items-center justify-between h-24">
+          {/* Hamburger button and Logo */}
+          <div className="flex items-center">
             {/* Hamburger Icon */}
             <button
               type="button"
               onClick={handleMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="md:hidden inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-label={open ? "Close Main Menu" : "Open Main Menu"}
               aria-expanded={open}
             >
               {open ? <FaTimes /> : <FaBars />}
             </button>
 
-            <div className="flex items-center">
-              <Link to="/">
-                <img
-                  src={logo_image}
-                  alt="SmartCART Logo"
-                  className="w-20 h-20 lg:w-40 lg:h-40 rounded-full"
-                />
-              </Link>
-            </div>
+            <Link to="/">
+              <img
+                src={logo_image}
+                alt="SmartCART Logo"
+                className="w-20 h-20 lg:w-40 lg:h-40 rounded-full ml-4 md:ml-0"
+              />
+            </Link>
           </div>
+
           {/* Emojis */}
-          <div className="flex items-center space-x-2 md:hidden">
+          <div className="flex items-center space-x-4">
             <FiSun className="text-black text-lg" />
             <FiMoon className="text-black text-lg" />
-            <HiOutlineShoppingCart className="text-black text-lg" />
+            <Link to="/cart">
+              <div className="relative">
+                <HiOutlineShoppingCart className="text-black text-lg" />
+                {cartLength > 0 && (
+                  <div className="absolute -top-1 right-0 transform translate-x-2 -translate-y-2">
+                    <div className="rounded-full bg-red-500 text-white w-5 h-5 text-xs font-bold flex items-center justify-center">
+                      {cartLength}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Link>
             <FaUser className="text-black text-lg" />
           </div>
 
@@ -76,7 +101,7 @@ const Navbar = () => {
               {navLinks.map((link, index) => (
                 <Link
                   key={index}
-                  className="text-black transition-all duration-500 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  className="text-black transition-all duration-500 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium"
                   to={link.link}
                 >
                   {link.title}
@@ -86,14 +111,15 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      {/* mobile - menu */}
+
+      {/* Mobile Menu */}
       {open && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             {navLinks.map((link, index) => (
               <Link
                 key={index}
-                className="text-black hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                className="text-black hover:bg-gray-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 to={link.link}
               >
                 {link.title}
@@ -118,30 +144,4 @@ export default Navbar;
         <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
         Aprill@gmail.com
         </span>
-
-  {
-    title: "Contact-Us",
-    link: "/contact-us",
-  },
-  {
-    title: "Meet the Developers",
-    link: "/meet-the-developers",
-  },
-  {
-    title: "Feedback",
-    link: "/",
-  },
-  {
-    title: "Review A Store",
-    link: "/",
-  },
-  {
-    title: "Profile Settings",
-    link: "/",
-  },
-  {
-    title: "Sign Out",
-    link: "/",
-  },
- 
 */
