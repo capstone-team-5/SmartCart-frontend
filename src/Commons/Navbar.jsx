30ad1c51b31f2b1e54136ee1dde97976a4666ea0
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-06 20:03:28 -0400

    error in css click fixed

--- src/Commons/Navbar.jsx
+++ src/Commons/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = ({ cartLength }) => {
             <button
               type="button"
               onClick={handleMenu}
-              className=" inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white transition duration-300 ease-in-out transform hover:scale-105"
               aria-label={open ? "Close Main Menu" : "Open Main Menu"}
               aria-expanded={open}
             >
@@ -111,44 +111,56 @@ const Navbar = ({ cartLength }) => {
 
           {/* Emojis */}
           <div className="flex items-center space-x-4 md:space-x-8 lg:space-x-16">
-            <span className="hover:bg-gray-100 p-2 rounded-md group relative">
+            <span className="hover:bg-gray-100 p-2 rounded-md group relative transition duration-300 ease-in-out transform hover:scale-105">
               <FiSun className="text-black text-lg lg:text-3xl md:text-xl sm:text-md group-hover:text-gray-100" />
               <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black py-1 px-2 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                 Light Mode
               </span>
             </span>
 
-            <span className="hover:bg-gray-100 p-2 rounded-md group relative">
+            <span className="hover:bg-gray-100 p-2 rounded-md group relative transition duration-300 ease-in-out transform hover:scale-105">
               <FiMoon className="text-black text-lg lg:text-3xl md:text-xl sm:text-md group-hover:text-gray-100" />
               <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black py-1 px-2 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                 Dark Mode
               </span>
             </span>
 
-            <span className="hover:bg-gray-100 p-2 rounded-md group relative">
+            <span className="hover:bg-gray-100 p-2 rounded-md group relative transition duration-300 ease-in-out transform hover:scale-105">
               <FaHeart className="text-orange-500 text-lg lg:text-3xl md:text-xl sm:text-md group-hover:text-gray-100" />
               <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black py-1 px-2 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                 My Favorites
               </span>
             </span>
 
-            <Link to="/cart">
-              <div className="relative">
-                <HiOutlineShoppingCart className="text-black text-lg lg:text-3xl md:text-xl sm:text-md hover:bg-gray-100 rounded-md" />
+            <Link
+              to="/cart"
+              className="hover:bg-gray-100 p-2 rounded-md group relative transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              <div className="relative group">
+                <HiOutlineShoppingCart className="text-black text-lg lg:text-3xl md:text-xl sm:text-md group-hover:text-gray-100" />
+                <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black py-1 px-2 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                  My Cart
+                </span>
                 {cartLength > 0 && (
                   <div className="absolute -top-1 right-0 transform translate-x-2 -translate-y-2">
-                    <div className="rounded-full bg-orange-500 text-white w-5 h-5 text-xs font-bold flex items-center justify-center">
+                    <div className="rounded-full bg-orange-500 text-black w-5 h-5 text-xs font-bold flex items-center justify-center">
                       {cartLength}
                     </div>
                   </div>
                 )}
               </div>
             </Link>
-            <div className="relative">
-              <PiUserCircle
-                className="text-black text-2xl cursor-pointer lg:text-3xl md:text-xl sm:text-md hover:bg-gray-100 rounded-md"
+
+            <div className="relative p-2 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <div
+                className={`text-black text-2xl cursor-pointer lg:text-3xl md:text-xl sm:text-md ${showUserDropdown}`}
                 onClick={toggleUserDropdown}
-              />
+              >
+                <PiUserCircle />
+              </div>
+              {/* <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-100 text-black py-1 px-2 rounded-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                My Account
+              </span> */}
               {showUserDropdown && (
                 <div className="absolute mt-2 right-0">
                   <ul className="py-1">
@@ -156,7 +168,7 @@ const Navbar = ({ cartLength }) => {
                       <li key={index} className="px-4 py-2 hover:bg-gray-100">
                         <Link
                           to={item.link}
-                          className="text-black block px-3 py-2 rounded-md text-base font-medium whitespace-nowrap "
+                          className="text-black block px-3 py-2 rounded-md text-base font-medium whitespace-nowrap"
                         >
                           {item.title}
                         </Link>
