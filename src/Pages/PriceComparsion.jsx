138dfa8dfe72226e478c0efa062e283bd0c8f271
Diandre Miller <millerdiandre@gmail.com>
2023-09-08 04:46:51 -0400

    price compare component added

---
+++ src/Pages/PriceComparsion.jsx
@@ -0,0 +1,14 @@
+//This is the price comparison page
+
+import PriceComparisonComponent from "../MVPComponents/PriceComparisonComponent"
+
+const PriceComparison = () => {
+
+    return (
+        <div>
+            <PriceComparisonComponent />
+        </div>
+    )
+};
+
+export default PriceComparison;
