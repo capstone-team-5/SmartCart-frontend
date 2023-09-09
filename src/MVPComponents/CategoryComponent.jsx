7eb971ecf2b199430ea337f18eb0a2af1cdc8646
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-09 16:25:04 -0400

    add unique categories

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -4,117 +4,51 @@ import { useState, useEffect } from "react";
 
 const CategoryComponent = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [grocery, setGrocery] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
+    // Fetch all products from backend and extract only unique categories
     fetch(`${process.env.REACT_APP_BACKEND_API}/products`)
       .then((response) => response.json())
       .then((data) => {
-        setGrocery(data);
+        const uniqueCategories = [
+          ...new Set(data.map((item) => item.product_category)),
+        ];
+        setCategories(uniqueCategories);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   }, []);
 
-  useEffect(() => {
-    if (selectedCategory) {
-      const filtered = grocery.filter(
-        (product) => product.category === selectedCategory
-      );
-      setFilteredProducts(filtered);
-    } else {
-      setFilteredProducts(grocery);
-    }
-  }, [selectedCategory, grocery]);
-
-  const handleCategoryClick = (category) => {
+  console.log(categories);
+  // Function to handle category selection
+  const handleCategorySelection = (category) => {
     setSelectedCategory(category);
+    console.log(selectedCategory);
   };
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md ">
-          <button
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-            onClick={() => handleCategoryClick("Dairy")}
+      {categories.length > 0 ? (
+        categories.map((category) => (
+          <div
+            key={category}
+            className="flex flex-col items-center"
+            onClick={() => handleCategorySelection(category)}
           >
-            Dairy
-          </button>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("Vegetables")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Vegetables
-          </button>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("fruits")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Fruits
-          </button>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("Bakery")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Bakery
-          </button>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Canned Goods
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Meat
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Beverages
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide ">
-            Snacks
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Frozen Foods
-          </button>
-        </div>
-      </div>
+            <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
+              <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+                {category}
+              </button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No categories found!</p>
+      )}
     </div>
   );
 };
+
 export default CategoryComponent;
