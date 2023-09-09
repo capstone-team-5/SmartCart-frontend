6c1a39f047e124b7a1c4a5d8d407691b9a9d37c7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-09 16:56:48 -0400

    add 2 state for fetching products and filteringg products

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -3,8 +3,10 @@
 import { useState, useEffect } from "react";
 
 const CategoryComponent = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(""); // to choose which categgory user selects
+  const [categories, setCategories] = useState([]); // to fetch unique categories from backend
+  const [products, setProducts] = useState([]); // to fetch all products from backend
+  const [filteredProducts, setFilteredProducts] = useState([]); // to filter products based on selected categgory
 
   useEffect(() => {
     // Fetch all products from backend and extract only unique categories
@@ -38,7 +40,10 @@ const CategoryComponent = () => {
             onClick={() => handleCategorySelection(category)}
           >
             <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-              <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
+              <button
+                className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-4"
+                onClick={() => handleCategorySelection(category)}
+              >
                 {category}
               </button>
             </div>
