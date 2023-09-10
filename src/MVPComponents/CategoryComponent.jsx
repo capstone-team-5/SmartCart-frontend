d1a6db4cacfe26a8802cf0606f1b707759f5ef5b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-09 22:00:43 -0400

    categories and render filter results works

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -1,15 +1,17 @@
 // This function will list all categories
 
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import FilterResultsComponent from "./FilterResultsComponent";
 
 const CategoryComponent = () => {
   const [selectedCategory, setSelectedCategory] = useState(""); // to choose which category user selects
   const [categories, setCategories] = useState([]); // to fetch unique categories from backend
   const [products, setProducts] = useState([]); // to fetch all products from backend
   const [filteredProducts, setFilteredProducts] = useState([]); // to filter products based on selected category
+  const [isLoading, setIsLoading] = useState(true);
 
-  const { filteredCategory } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch all products from backend and extract only unique categories to categories
@@ -21,9 +23,11 @@ const CategoryComponent = () => {
         ];
         setCategories(uniqueCategories);
         setProducts(data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setIsLoading(false);
       });
   }, []);
 
@@ -39,40 +43,41 @@ const CategoryComponent = () => {
 
   // Function to handle category selection
   const handleCategorySelection = (category) => {
-    setSelectedCategory(category);
+    navigate(`/filter-results?category=${category}`);
   };
 
   return (
     <div>
-      {/* Render the category buttons */}
-      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        {categories.length > 0 ? (
-          categories.map((category) => (
-            <div
-              key={category}
-              className="flex flex-col items-center"
-              onClick={() => handleCategorySelection(category)}
-            >
-              <div className="bg-green-50 rounded-full w-44 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-                <button
-                  className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-0  w-full"
-                  onClick={() => handleCategorySelection(category)}
-                >
-                  {category}
-                </button>
+      {isLoading ? (
+        // Render a loading indicator (e.g., a spinner or loading message)
+        <p>Loading...</p>
+      ) : (
+        // Render the category buttons
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+          {categories.length > 0 ? (
+            categories.map((category) => (
+              <div key={category} className="flex flex-col items-center">
+                <div className="bg-green-50 rounded-full w-44 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
+                  <button
+                    className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-0  w-full"
+                    onClick={() => handleCategorySelection(category)}
+                  >
+                    {category}
+                  </button>
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p>No Categories Found!</p>
-        )}
-      </div>
+            ))
+          ) : (
+            <p>No Categories Found!</p>
+          )}
+        </div>
+      )}
       {/* Render the products based on selected category */}
-      {selectedCategory ? (
+      {selectedCategory && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-lg shadow-green-500/50 border shadow-md">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((item) => (
-              <FilterResults
+              <FilterResultsComponent
                 key={item.product_id}
                 filteredCategory={selectedCategory}
               />
@@ -81,8 +86,6 @@ const CategoryComponent = () => {
             <p>No products found for the selected category.</p>
           )}
         </div>
-      ) : (
-        <p>Please select a category.</p>
       )}
     </div>
   );
