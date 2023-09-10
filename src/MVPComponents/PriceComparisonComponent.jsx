c37139e2e11e574479d6f7ef2ae8196b75780a20
Diandre Miller <millerdiandre@gmail.com>
2023-09-10 16:03:57 -0400

    updated state in cart

--- src/MVPComponents/PriceComparisonComponent.jsx
+++ src/MVPComponents/PriceComparisonComponent.jsx
@@ -1,6 +1,15 @@
 //This component will compare items across carts
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
 const PriceComparisonComponent = () => {
+
+    // useEffect(() => {
+    //     axios.get()
+
+    // },[])
     return (
         <div>
             <h3>This is the price comparison component</h3>
