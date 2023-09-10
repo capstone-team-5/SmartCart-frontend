a898582f5e5a076879607af40928e3f21196be1c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-09 20:34:19 -0400

    create filter results display results based on user selected category user and from filter side bar

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -1,12 +1,15 @@
 // This function will list all categories
 
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const CategoryComponent = () => {
-  const [selectedCategory, setSelectedCategory] = useState(""); // to choose which categgory user selects
+  const [selectedCategory, setSelectedCategory] = useState(""); // to choose which category user selects
   const [categories, setCategories] = useState([]); // to fetch unique categories from backend
   const [products, setProducts] = useState([]); // to fetch all products from backend
-  const [filteredProducts, setFilteredProducts] = useState([]); // to filter products based on selected categgory
+  const [filteredProducts, setFilteredProducts] = useState([]); // to filter products based on selected category
+
+  const { filteredCategory } = useParams();
 
   useEffect(() => {
     // Fetch all products from backend and extract only unique categories to categories
@@ -41,6 +44,7 @@ const CategoryComponent = () => {
 
   return (
     <div>
+      {/* Render the category buttons */}
       <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
         {categories.length > 0 ? (
           categories.map((category) => (
@@ -49,9 +53,9 @@ const CategoryComponent = () => {
               className="flex flex-col items-center"
               onClick={() => handleCategorySelection(category)}
             >
-              <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
+              <div className="bg-green-50 rounded-full w-44 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
                 <button
-                  className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-4"
+                  className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-0  w-full"
                   onClick={() => handleCategorySelection(category)}
                 >
                   {category}
@@ -60,24 +64,26 @@ const CategoryComponent = () => {
             </div>
           ))
         ) : (
-          <p>No categories found!</p>
+          <p>No Categories Found!</p>
         )}
       </div>
-
-      <div>
-        {/* Render the filtered products */}
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            <div key={product.product_id}>
-              {/* Rendering the product details */}
-              <p>{product.product_name}</p>
-              <p>{product.product_description}</p>
-            </div>
-          ))
-        ) : (
-          <p>No products found for the selected category.</p>
-        )}
-      </div>
+      {/* Render the products based on selected category */}
+      {selectedCategory ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-lg shadow-green-500/50 border shadow-md">
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((item) => (
+              <FilterResults
+                key={item.product_id}
+                filteredCategory={selectedCategory}
+              />
+            ))
+          ) : (
+            <p>No products found for the selected category.</p>
+          )}
+        </div>
+      ) : (
+        <p>Please select a category.</p>
+      )}
     </div>
   );
 };
--- src/MVPComponents/FilterResultsComponent.jsx
+++ src/MVPComponents/FilterResultsComponent.jsx
@@ -1,49 +1,9 @@
-//This function will display results after user has applied filters
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
+// This function will display results based on user selected category user and from filter side bar
 
 const FilterResultsComponent = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const selectedFilters = queryParams.getAll("filters");
-  const [allProducts, setAllProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_API}/products`
-        );
-        setAllProducts(response.data);
-
-        // Filter the products based on selected checkboxes
-        const updatedFilteredProducts = response.data.filter((product) => {
-          return selectedFilters.every((filter) => {
-            const filterField = `product_is_${filter.toLowerCase()}`;
-            return product[filterField];
-          });
-        });
-        setFilteredProducts(updatedFilteredProducts);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-    fetchProducts();
-  }, [selectedFilters]);
-
   return (
     <div>
-      <div className="grid grid-cols-3 gap-2 border border-blue-500">
-        {filteredProducts.map((product) => (
-          <div key={product.product_id}>
-            <img src={product.product_image} alt={product.product_name} />
-            <p>{product.product_name}</p>
-            <p>{product.product_description}</p>
-          </div>
-        ))}
-      </div>
+      <h3>This is the about us page</h3>
     </div>
   );
 };
