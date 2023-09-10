1c1b05529aa9a901ff1f12fab5995c581870a88c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-09 22:01:33 -0400

    set route for results

--- src/MVPComponents/FilterResultsComponent.jsx
+++ src/MVPComponents/FilterResultsComponent.jsx
@@ -1,9 +1,14 @@
 // This function will display results based on user selected category user and from filter side bar
+import { useLocation } from "react-router-dom";
 
 const FilterResultsComponent = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const selectedCategory = queryParams.get("category");
+
   return (
     <div>
-      <h3>This is the about us page</h3>
+      <h3>This is the results page</h3>
     </div>
   );
 };
