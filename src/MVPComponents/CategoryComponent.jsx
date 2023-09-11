244a8f72dacc9cbdd361c43f0d3df68dd5a99000
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-11 02:20:07 -0400

    assign /filter instead of /fil

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -31,7 +31,7 @@ const CategoryComponent = () => {
   // Function to handle category selection
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-    navigate(`/fil?category=${category}`);
+    navigate(`/filter?category=${category}`);
   };
 
   return (
