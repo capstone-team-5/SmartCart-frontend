59f360c464277628bd5cc8e30724febb129b9bd3
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-10 23:38:57 -0400

    add location and params

--- src/Pages/FilterResults.jsx
+++ src/Pages/FilterResults.jsx
@@ -1,14 +1,20 @@
 import FilterResultsComponent from "../MVPComponents/FilterResultsComponent";
 import FilterSideBarComponent from "../MVPComponents/FilterSideBarComponent";
-
-const FilterResults = () => {
+import { useLocation } from "react-router-dom";
+const FilterResults = ({ addToCart }) => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const selectedCategory = queryParams.get("category");
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="col-span-1">
         <FilterSideBarComponent />
       </div>
       <div className="col-span-1">
-        <FilterResultsComponent />
+        <FilterResultsComponent
+          addToCart={addToCart}
+          selectedCategory={selectedCategory}
+        />
       </div>
     </div>
   );
