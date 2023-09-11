56d1f9799ecc4ee13b2f59b7bfe3537393957788
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-11 05:29:10 -0400

    added props to compoenent

--- src/Pages/FilterResults.jsx
+++ src/Pages/FilterResults.jsx
@@ -1,14 +1,14 @@
 import FilterResultsComponent from "../MVPComponents/FilterResultsComponent";
 import FilterSideBarComponent from "../MVPComponents/FilterSideBarComponent";
 import { useLocation } from "react-router-dom";
-const FilterResults = ({ addToCart }) => {
+const FilterResults = ({ addToCart, applyFilters }) => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const selectedCategory = queryParams.get("category");
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="col-span-1">
-        <FilterSideBarComponent />
+        <FilterSideBarComponent applyFilters={applyFilters} />
       </div>
       <div className="col-span-1">
         <FilterResultsComponent
