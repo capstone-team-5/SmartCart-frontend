3a6dc8d4dd293acf6225e22d302ac21065ebb570
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-12 02:21:19 -0400

    changged the wrong import

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import FilterResultsComponent from "./Rejected";
+import FilterResultsComponent from "./FilterResultsComponent";
 import axios from "axios";
 
 const CategoryComponent = () => {
