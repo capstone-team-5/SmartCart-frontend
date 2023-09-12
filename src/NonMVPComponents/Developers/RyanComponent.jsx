0d50ab4d163bd30b5c95fce9af302177804836eb
ryanfodera <ryanfodera@pursuit.org>
2023-09-12 12:10:47 -0400

    user ID changed from local to table to delete component, Ryan component gramtically fixed, sign up component updated as well

--- src/NonMVPComponents/Developers/RyanComponent.jsx
+++ src/NonMVPComponents/Developers/RyanComponent.jsx
@@ -6,7 +6,7 @@ const RyanComponent = () => {
             <h3>
                 <h3>
                 My name is Ryan Fodera, and I'm an aspiring full-stack web developer who is currently 25 years old and lives in Staten Island, New York. I am a graduate of the College of Charleston with a bachelor's degree in Sociology and a minor in Urban Studies. I have a passion for learning and am always looking to acquire new knowledge, which is why I decided to change my career path to the world of coding after completing college. My hobbies include playing video games, and engaging in and watching sports, specifically baseball and basketball. I enjoy spending quality time with my family and friends. My favorite quote is "Stay hungry, stay foolish" by Steve Jobs. This quote is important to me because it reminds me to never become lazy or complacent and to always push myself to try new things, even if it means taking a risk.
-                    </h3>
+                </h3>
             </h3>
         </div>
     )
