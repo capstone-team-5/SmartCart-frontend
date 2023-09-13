1ca5cfbbff691841de81257ad056c0ce844d53b2
Tafari Excell <excell.tafari@gmail.com>
2023-09-13 17:51:20 -0400

    Added DeleteButtonComponent to UserComponent

--- src/MVPComponents/UserComponent.jsx
+++ src/MVPComponents/UserComponent.jsx
@@ -1,13 +1,14 @@
 //This component is for the user
 import React from 'react';
-import SearchComponent from './SearchComponent.jsx';
 import DeleteButtonComponent from './DeleteButtonComponent.jsx';
 const UserComponent = () => {
+
     return (
         <div>
-            <SearchComponent />
+        
+         
             <DeleteButtonComponent />
-            <h3>This is the user component</h3>
+           
         </div>
     )
 };
