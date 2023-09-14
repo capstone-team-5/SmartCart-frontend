535c66abc2ac547422b485fb13826b7c1ce5ca54
Tafari Excell <excell.tafari@gmail.com>
2023-09-14 19:31:21 -0400

    Changed class syntax to className

--- src/MVPComponents/UserComponent.jsx
+++ src/MVPComponents/UserComponent.jsx
@@ -1,16 +1,16 @@
 //This component is for the user
 import React from 'react';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 const UserComponent = () => {
-    
-
+   
     return (     
         
         <div>
-            <h1 class="text-4xl"><strong>Account Settings</strong></h1>
+            <h1 className="text-4xl"><strong>Account Settings</strong></h1>
             <br>
             </br>
-            <h2 class="text-2xl">Account Information</h2>
+            <h2 className="text-4xl">Account Information</h2>
             <br>
             </br>
             <label for="email">Email</label>
