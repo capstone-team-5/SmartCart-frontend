3fcb7896ea632e9d7a31a401dd30efd4c8ab9135
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-14 05:08:08 -0400

    change password component with css, validations, darkmode

--- src/NonMVPComponents/ChangePasswordComponent.jsx
+++ src/NonMVPComponents/ChangePasswordComponent.jsx
@@ -1,20 +1,47 @@
-// This function will help change password
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const ChangePasswordComponent = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === confirmPassword) {
+      setFormSubmitted(true);
+      setPasswordError("");
+    } else {
+      setPasswordError("Passwords do not match");
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
   return (
-    <section className=" dark:bg-gray-900">
+    <section className="">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
           <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
             Change Password
           </h2>
-          <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+          <form
+            className="mt-4 space-y-4 lg:mt-5 md:space-y-5"
+            onSubmit={handleSubmit}
+          >
             <div>
               <label
-                htmlfor="email"
+                htmlFor="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your email
+                Email:
               </label>
               <input
                 type="email"
@@ -28,7 +55,7 @@ const ChangePasswordComponent = () => {
             </div>
             <div>
               <label
-                htmlfor="password"
+                htmlFor="password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 New Password
@@ -38,24 +65,30 @@ const ChangePasswordComponent = () => {
                 name="password"
                 id="password"
                 placeholder="••••••••"
+                autoComplete="off"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
+                value={password} // Bind the value to the state
+                onChange={handlePasswordChange} // Update the state on change
               />
             </div>
             <div>
               <label
-                htmlfor="confirm-password"
+                htmlFor="confirm-password"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Confirm password
               </label>
               <input
-                type="confirm-password"
+                type="password"
                 name="confirm-password"
                 id="confirm-password"
                 placeholder="••••••••"
+                autoComplete="off"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
+                value={confirmPassword} // Bind the value to the state
+                onChange={handleConfirmPasswordChange} // Update the state on change
               />
             </div>
             <div className="flex items-start">
@@ -70,7 +103,7 @@ const ChangePasswordComponent = () => {
               </div>
               <div className="ml-3 text-sm">
                 <label
-                  htmlfor="newsletter"
+                  htmlFor="newsletter"
                   className="font-light text-gray-500 dark:text-gray-300"
                 >
                   I accept the{" "}
@@ -90,6 +123,20 @@ const ChangePasswordComponent = () => {
               Change Password
             </button>
           </form>
+          {passwordError && (
+            <div className="mt-4 text-center">
+              <p className="text-red-600">{passwordError}</p>
+            </div>
+          )}
+          {formSubmitted && password === confirmPassword ? (
+            <div className="mt-4 text-center">
+              <p className="text-green-600">Password changed successfully!</p>
+            </div>
+          ) : formSubmitted && password !== confirmPassword ? (
+            <div className="mt-4 text-center">
+              <p className="text-red-600">Passwords do not match.</p>
+            </div>
+          ) : null}
         </div>
       </div>
     </section>
