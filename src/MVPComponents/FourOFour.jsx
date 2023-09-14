80b50d78adb604e0fa5300897797ecfa21f8850b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-14 03:14:17 -0400

    edit css and dark mode to 404

--- src/MVPComponents/FourOFour.jsx
+++ src/MVPComponents/FourOFour.jsx
@@ -1,32 +1,30 @@
 //  This function is the error page, will take user back to the home page
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const FourOFourComponent = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4 md:px-8">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 text-center">
-        Page Not Found !!!
-      </h1>
-      <p className="text-gray-600 mb-8 text-center">
-        The page you're looking for may have been moved or doesn't exist.
-      </p>
-      <Link
-        to="/"
-        className="bg-blue-500 text-white px-4 py-2 rounded-full shadow-md hover:bg-blue-600 transition duration-300 text-sm md:text-base lg:text-lg"
-        aria-label="Go to Home Page"
-      >
-        Home
-      </Link>
-      <button
-        onClick={() => navigate(-1)}
-        className="mt-4 text-gray-600 hover:text-gray-800 text-sm md:text-base lg:text-lg"
-        aria-label="Go Back"
-      >
-        Back
-      </button>
-    </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-4xl tracking-tight font-extrabold lg:text-6xl text-primary-600 dark:text-white">
+            Page Not Found !!!
+          </h1>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.{" "}
+          </p>
+          <button
+            onClick={() => navigate(-1)}
+            className="inline-flex text-black dark:text-white bg-blue-300 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+            aria-label="Go Back"
+          >
+            Back to Homepage
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
