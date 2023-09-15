cc59b9339049c3d8173d63f1263ebb84b8ea4107
Tafari Excell <excell.tafari@gmail.com>
2023-09-15 00:24:28 -0400

    imported useState into component. Created handleTexhChange functionality into component.HandleTexhChange function takes care of text changes in input fieds label Email, Password, Name and Phone

--- src/MVPComponents/UserComponent.jsx
+++ src/MVPComponents/UserComponent.jsx
@@ -1,35 +1,64 @@
 //This component is for the user
 import React from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+
 const UserComponent = () => {
+    const [formData, setFormData] = useState({
+        email:'', 
+        password:'', 
+        name:'', 
+        phone:''
+    });
+    const handleTextChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    };
    
+   
+        
     return (     
         
         <div>
             <h1 className="text-4xl"><strong>Account Settings</strong></h1>
-            <br>
-            </br>
+            <br />
             <h2 className="text-2xl">Account Information</h2>
-            <br>
-            </br>
-            <label for="email">Email</label>
-            <br></br>
-            <input type="text" placeholder="" />
-            <br></br>
-            <label for="password">Password</label>
-            <br></br>
-            <input type="text" placeholder="" />
-            <br></br>
+            <br />
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="text" 
+            placeholder=""
+            name="email"
+            value={formData.email} 
+            onchange={handleTextChange}/>
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="text" 
+            placeholder="" 
+            name="password"
+            value={formData.password} 
+            onChange={handleTextChange}/>
+            <br />
             <h2 className="text-2xl">Personal Information</h2>
-            <br></br>
-            <label for="name">Name</label>
-            <br></br>
-            <input type="text" placeholder="" />
-            <br></br>
-            <label for="phone">Phone Number</label>
-            <br></br>
-            <input tpye="text" placeholder="xxx-xxx-xxxx" />
+            <br />
+            <label htmlFor="name">Name</label>
+            <br />
+            <input type="text" 
+            placeholder=""
+            name="name" 
+            value={formData.name} 
+            onChange={handleTextChange} />
+            <br />
+            <label htmlFor="phone">Phone Number</label>
+            <br />
+            <input type="text" 
+            placeholder="xxx-xxx-xxxx"
+            name="phone" 
+            value={formData.phone} 
+            onChange={handleTextChange} />
         </div>
     
     )
