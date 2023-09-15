cfa3d1458461f28b6407b98ec50f229ab9158218
Tafari Excell <excell.tafari@gmail.com>
2023-09-15 04:37:04 -0400

    Added deltebutton to component with user friendly alert window to confirm user want to delete profile. Imported react, useParams, useNavigate and axios into component. Added a catch error to assist in catching any errors and produce error message to user when an error occurs.

--- src/MVPComponents/DeleteButtonComponent.jsx
+++ src/MVPComponents/DeleteButtonComponent.jsx
@@ -1,23 +1,36 @@
 //This function is a delete button
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const DeleteButtonComponent = () => {
-   
-
-        const handleDelete = () => {
-            // Code to handle the delete action goes here
-            console.log("");
-        
     
-            
-        
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        // Code to handle the delete action goes here
+       const confirmed = window.confirm("Are you you want to delete this user profile?")
+       if(confirmed) {
+        axios.delete(`${process.env.REACT_APP_BACKEND_API}/users/${id}`)
+            .then((response) => {
+                console.log(response.data);
+                navigate(`/users`);
+            }
+            )
+            .catch((error) => {
+                console.error(error);
+            }
+            );
+
     }
+}
     return (
         <div>
             <button onClick={handleDelete}>Delete</button>
-           
+          
         </div>
     )
-    
-
 }
 export default DeleteButtonComponent;
