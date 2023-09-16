89710fe0b923031db16b27471e85f9d7e999a51c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-16 17:04:26 -0400

    erase comment

--- src/Pages/CategoryPage.jsx
+++ src/Pages/CategoryPage.jsx
@@ -26,7 +26,7 @@ export default CategoryPage;
         class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
       >
         <span class="sr-only">Open sidebar</span>
-        <svg
+        <svg // filter button
           class="w-6 h-6"
           aria-hidden="true"
           fill="currentColor"
