757fb842bc4f72275e1ec0ac9612588c1e046867
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-16 15:19:18 -0400

    changed css ggrid

--- src/MVPComponents/FilterResultsComponent.jsx
+++ src/MVPComponents/FilterResultsComponent.jsx
@@ -7,28 +7,28 @@ const FilterResultsComponent = ({ selectedCategory }) => {
   return (
     <div>
       <h3>Shopping in {selectedCategory[0].product_category} category</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
-        {selectedCategory.map((product) => (
-          <div
-            key={product.product_id}
-            className="bg-white rounded-lg shadow-md overflow-hidden"
-          >
-            <img
-              src={product.product_image}
-              alt={product.product_name}
-              className="w-full h-48 object-contain"
-            />
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+          <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"></div>
+          {selectedCategory.map((product) => (
+            <div
+              key={product.product_id}
+              className="bg-white rounded-lg shadow-md overflow-hidden"
+            >
+              <img
+                src={product.product_image}
+                alt={product.product_name}
+                className="w-full h-48 object-contain"
+              />
 
-            <div className="p-4">
-              <h2 className="text-lg text-center font-semibold">
-                {product.product_name}
-              </h2>
+              <div className="p-4">
+                <h2 className="text-lg text-center font-semibold">
+                  {product.product_name}
+                </h2>
+              </div>
             </div>
-            {/* <button onClick={() => addToCart(product)}>
-                  Add To Cart
-                </button> */}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
