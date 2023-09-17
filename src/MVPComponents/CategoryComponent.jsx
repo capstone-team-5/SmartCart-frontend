b5e64cfbca4a62930f4c8c54d090797d6983f3fb
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-17 17:17:37 -0400

    pass props

--- src/MVPComponents/CategoryComponent.jsx
+++ src/MVPComponents/CategoryComponent.jsx
@@ -4,7 +4,7 @@ import FilterResultsComponent from "./FilterResultsComponent";
 import { Link } from "react-scroll";
 import axios from "axios";
 
-const CategoryComponent = () => {
+const CategoryComponent = ({ appliedFilters }) => {
   const [selectedCategory, setSelectedCategory] = useState(""); // to choose which category user selects
   const [categories, setCategories] = useState([]); // to fetch categories from backend
   const [isLoading, setIsLoading] = useState(true);
@@ -42,45 +42,48 @@ const CategoryComponent = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div id="categorySection">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
-          <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {categories.length > 0 ? (
-              categories.map((categoryObj) => (
-                <div
-                  key={categoryObj.product_category}
-                  className="text-center text-gray-500 dark:text-gray-400"
-                >
-                  <div className="bg-green-50 rounded w-44 h-40 flex items-center justify-center shadow-green-500/50 shadow-md">
-                    <Link
-                      activeClass="active"
-                      className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-0 w-full cursor-pointer"
-                      to="filterResults"
-                      spy={true}
-                      smooth={true}
-                      offset={-60} // Adjust the offset as needed
-                      duration={500}
-                      onClick={() =>
-                        handleCategoryClick(categoryObj.product_category)
-                      }
-                    >
-                      {categoryObj.product_category}
-                    </Link>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+            <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {categories.length > 0 ? (
+                categories.map((categoryObj) => (
+                  <div
+                    key={categoryObj.product_category}
+                    className="text-center text-gray-500 dark:text-gray-400"
+                  >
+                    <div className="bg-green-50 rounded w-44 h-40 flex items-center justify-center shadow-green-500/50 shadow-md">
+                      <Link
+                        activeClass="active"
+                        className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide p-0 w-full cursor-pointer"
+                        to="filterResults"
+                        spy={true}
+                        smooth={true}
+                        offset={-60}
+                        duration={500}
+                        onClick={() =>
+                          handleCategoryClick(categoryObj.product_category)
+                        }
+                      >
+                        {categoryObj.product_category}
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              ))
-            ) : (
-              <p>No Categories Found!</p>
-            )}
+                ))
+              ) : (
+                <p>No Categories Found!</p>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
       <div id="filterResults">
         {selectedCategory && (
-          <FilterResultsComponent selectedCategory={selectedCategory} />
+          <FilterResultsComponent
+            selectedCategory={selectedCategory}
+            appliedFilters={appliedFilters}
+          />
         )}
       </div>
     </section>
