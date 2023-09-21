b145f9b902398a2eef94cdc1db88779f82d44184
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-21 16:36:37 -0400

    resolved conflict in individual product

--- src/MVPComponents/IndividualProductComponent.jsx
+++ src/MVPComponents/IndividualProductComponent.jsx
@@ -6,38 +6,38 @@ import { AiFillStar } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 import axios from "axios";
 
-const IndividualProductComponent = ({ item,  handleAddToCart }) => {
-  const [oneItem, setitem] = useState({});
+const IndividualProductComponent = ({ handleAddToCart }) => {
+  const [showOneItem, setShowOneItem] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_API}/products/${id}`)
-      .then((response) => setitem(response.data))
+      .then((response) => setShowOneItem(response.data))
       .catch((error) => console.log(error));
   }, [id]);
 
   return (
     <div>
-      {item && (
+      {showOneItem && (
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <img
-            className="p-8 rounded-t-lg object-contain h-10"
-            src={item.product_image}
-            alt={item.product_name}
+            className="p-8 rounded-t-lg object-contain"
+            src={showOneItem.product_image}
+            alt={showOneItem.product_name}
           />
 
           <div className="px-4 py-2">
             <div className="flex items-center justify-between">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                {item.product_name}
+                {showOneItem.product_name}
               </h3>
               <h6 className="text-md font-medium text-gray-600 dark:text-gray-300">
-                {item.product_weight} {item.product_unit}
+                {showOneItem.product_weight} {showOneItem.product_unit}
               </h6>
             </div>
             <p className="text-sm text-gray-700 dark:text-gray-400 mt-2">
-              {item.product_description}
+              {showOneItem.product_description}
             </p>
 
             <div className="flex items-center mt-3">
@@ -57,7 +57,7 @@ const IndividualProductComponent = ({ item,  handleAddToCart }) => {
             <div className="flex justify-end mt-4">
               <button
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                onClick={() => handleAddToCart(item)}
+                onClick={() => handleAddToCart(showOneItem)}
               >
                 Add To Cart
               </button>
