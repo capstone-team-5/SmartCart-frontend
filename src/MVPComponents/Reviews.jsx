d9b0712fe686570db5450ef2b9c0a06d68164f22
touhami-ben <brooklyn.ben76@gmail.com>
2023-09-21 08:35:08 -0400

    add handleAdd

--- src/MVPComponents/Reviews.jsx
+++ src/MVPComponents/Reviews.jsx
@@ -6,18 +6,45 @@ import ReviewForm from "./ReviewForm";
 
 const API = process.env.REACT_APP_BACKEND_API;
 
-function Review() {
+function Reviews() {
     const [reviews, setReviews] = useState([]);
     let { id } = useParams();
   
     useEffect(() => {
-      axios.get(`${API}/groceries/${id}/reviews`).then((response) => {
+      axios.get(`${API}/stores/${id}/reviews`).then((response) => {
         console.log(response.data);
         setReviews(response.data);
       });
     }, [id]);
 
-    return(<section>
+    const handleAdd = (newReview) => {
+        axios
+          .post(`${API}/stores/${id}/reviews`, newReview)
+          .then(
+            (response) => {
+              setReviews([response.data, ...reviews]);
+            },
+            (error) => console.error(error)
+          )
+          .catch((c) => console.warn("catch", c));
+      };
+    
 
-    </section>);
+    return(
+    <section className="">
+        <h2 className="">Reviews</h2>
+        <ReviewForm handleSubmit={handleAdd}>
+            <h3 className="">Add a new Review</h3>
+        </ReviewForm>
+        {reviews.map((review) => (
+            <Review
+            key={review.id}
+            review={review}
+            handleSubmit={""}
+            handelDelete={""}
+            />
+        ))}
+
+    </section>
+    );
 }
