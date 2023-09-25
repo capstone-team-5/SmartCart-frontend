3ce149caea04ccca894a19229bd897c9a6b7ca8e
Diandre Miller <millerdiandre@gmail.com>
2023-09-25 13:49:22 -0400

    recipes component added, terms and conditions updates

---
+++ src/NonMVPComponents/RecipesComponent.jsx
@@ -0,0 +1,13 @@
+//This component will allow users to view recipes and add all the ingredients from the recipe to the cart
+
+const RecipesComponent = () => {
+
+    return (
+        <div>
+            <h2>This is the recipes component</h2>
+        </div>
+    )
+};
+
+export default RecipesComponent;
+
---
+++ src/Pages/Recipes.jsx
@@ -0,0 +1,14 @@
+//This is the recipes page
+
+import RecipesComponent from "../NonMVPComponents/RecipesComponent"
+
+const Recipes = () => {
+
+    return (
+        <div>
+            <RecipesComponent />
+        </div>
+    )
+};
+
+export default Recipes;
