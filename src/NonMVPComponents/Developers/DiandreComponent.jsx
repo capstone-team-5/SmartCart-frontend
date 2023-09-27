6612f9e81c9e6f28ea8e6739c533396d4adc7841
Diandre Miller <millerdiandre@gmail.com>
2023-09-27 16:00:15 -0400

    updated about us

--- src/NonMVPComponents/Developers/DiandreComponent.jsx
+++ src/NonMVPComponents/Developers/DiandreComponent.jsx
@@ -1,48 +1,83 @@
 // This component will talk about Diandre
+
 import { Link } from "react-router-dom";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import Diandre from "../../Assets/DiandreMiller.jpg"
 const DiandreComponent = () => {
   return (
-    <div>
-      <p>
-        Hi my name is Diandre and I am 29 years. I am a first generation
-        American from Brooklyn, NY. I am currently a student at Pursuit learning
-        various programming languages and frameworks such as JavaScript, React,
-        Node, CSS, Bootstrap, and Express. I am also learning how to use various
-        tools such as Git, GitHub, and vscode. I truly enjoy building web
-        applications from scratch and the problem solving required to learn
-        these skills.
-        <br /> <br />
-        Prior to joining Pursuit, I had a background in journalism and political
-        science, so studying and learning new things is in my nature and has
-        always been a passion of mine. Outside of Pursuit, I am a gym
-        enthusiast. I've had various health issues throughout my life and many
-        things have been out of my control. So I decided to take control of my
-        health and fitness because it's something that I mostly have control of.
-        <br /> <br />
-        I actually decided to join Pursuit and get into software engineering
-        because of the various health issues and I was forced to switch careers.
-        It is seemingly a blessing in disguise. I am super grateful for what I
-        have learned so far and I am excited for my future in software
-        engineering.
-        <br /> <br />
-        I am also a huge sports fan. Basketball is my favorite sport, the lakers
-        are my favorite team, and Kobe Bryant (RIP) is my favorite player. I've
-        tried to adopt his mentality and work ethic daily to be the best
-        possible person I can be. Hopefully within the next year I will land a
-        job as a software engineer.
-        <br /> <br />
-        Check out my{" "}
-        <Link to="https://www.linkedin.com/in/diandre-miller/" target="_blank">
-          {" "}
-          LinkedIn
-        </Link>{" "}
-        and{" "}
-        <Link to="https://github.com/DiandreMiller" target="_blank">
-          {" "}
-          GitHub{" "}
-        </Link>{" "}
-        to see what I've been working on.
-      </p>
+    <div className="bg-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="mb-4 pb-4 p-4 text-center text-3xl font-bold dark:text-white">
+        {" "}
+        Diandre Miller
+      </h1>
+      <div className="flex flex-col items-center justify-center py-10">
+        {/* Central Image */}
+        <div className="relative">
+          <img
+            src={Diandre}
+            alt="Profile"
+            className="w-44 h-44 rounded-full transition transform hover:scale-110 animate-flyInUp"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center max-w-lg">
+        <div className="bg-purple-300 w-44 h-44 rounded-full m-2 p-4 flex items-center justify-center transition transform hover:scale-110 animate-flyInRight">
+          <p className="text-black text-center">
+            <strong>Hobbies</strong>
+            <br />
+            Basketball, Listening to Music
+          </p>
+        </div>
+        <div className="bg-yellow-300 w-44 h-44 rounded-full m-2 p-4 flex items-center justify-center transition transform hover:scale-110 animate-flyInLeft">
+          <p className="text-black text-center">
+            <strong>Passion</strong>
+            <br />
+            Coaching
+          </p>
+        </div>
+
+        <div className="bg-pink-300 w-44 h-44 rounded-full m-2 p-4 flex items-center justify-center transition transform hover:scale-110 animate-flyInRight">
+          <Link
+            to="https://github.com/Vandhana-Mohan"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black text-center mb-3"
+          >
+            <strong>Education</strong>
+            <br />
+            Journalism & Software Development
+          </Link>
+        </div>
+        <div className="bg-green-300 w-44 h-44 rounded-full m-2 p-4 flex items-center justify-center transition transform hover:scale-110 animate-flyInLeft">
+          <p className="text-black text-center">
+            <strong>Ask me about</strong>
+            <br />
+            Anything You Want
+          </p>
+        </div>
+        <div className="bg-orange-300 w-44 h-44 rounded-full m-2 p-4 flex flex-col items-center justify-center transition transform hover:scale-110 animate-flyInRight">
+          <Link
+            to="https://github.com/Vandhana-Mohan"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black text-center mb-3"
+          >
+            <strong>GitHub</strong>
+          </Link>
+          <BsGithub size={18} />
+        </div>
+        <div className="bg-blue-300 w-44 h-44 rounded-full m-2 p-4 flex flex-col items-center justify-center transition transform hover:scale-110 animate-flyInLeft">
+          <Link
+            to="https://www.linkedin.com/in/vandhanamohan/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black text-center mb-3"
+          >
+            <strong>LinkedIn</strong>
+          </Link>
+          <BsLinkedin size={18} />
+        </div>
+      </div>
     </div>
   );
 };
