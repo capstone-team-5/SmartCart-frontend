1a7344589ac6a800b2dde7e6e5f03c1532466fa1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-09-28 01:37:05 -0400

    home page grid

--- src/MVPComponents/HomeComponent/CategoryHome.jsx
+++ src/MVPComponents/HomeComponent/CategoryHome.jsx
@@ -1,69 +1,71 @@
-// This is the home component
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 const API = process.env.REACT_APP_BACKEND_API;
 
-const CategoryHome = ({ addToCart }) => {
-  const [products, setProducts] = useState([]);
+const CategoryHome = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [sections, setSections] = useState({
+    fruits: [],
+    vegetables: [],
+    spices: [],
+    dairy: [],
+  });
 
   useEffect(() => {
-    axios
-      .get(`${API}/products`)
-      .then((response) => {
-        setProducts(response.data);
+    const fetchData = async () => {
+      try {
+        const fruitsResponse = await axios.get(`${API}/products/fourFruits`);
+        const vegetablesResponse = await axios.get(`${API}/products/fourCart`);
+        const spicesResponse = await axios.get(`${API}/products/fourMRT`);
+        const dairyResponse = await axios.get(`${API}/products/fourGreens`);
+
+        setSections({
+          fruits: fruitsResponse.data,
+          vegetables: vegetablesResponse.data,
+          spices: spicesResponse.data,
+          dairy: dairyResponse.data,
+        });
+
         setLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error fetching data:", error);
         setError("An error occurred while fetching data.");
         setLoading(false);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const renderImages = (section) => {
+    return section.map((image, index) => (
+      <div key={index}>
+        <img
+          className="h-auto max-w-full rounded-lg"
+          src={image.product_image}
+          alt={`Art of ${index + 1}`}
+        />
+      </div>
+    ));
+  };
 
   if (loading) {
     return <p>Loading...</p>;
   }
+
   if (error) {
     return <p>Error: {error}</p>;
   }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 p-6">
       {/* Fruits Section */}
       <div className="bg-pink-200 p-4">
         <div className="text-xl font-bold text-center mb-4">Fruits</div>
         <div className="bg-blue-200 p-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
+          {renderImages(sections.fruits)}
         </div>
         <div className="bg-pink-200 p-4 text-center mt-4">
           <p className="font-bold">Shop More Fruits</p>
@@ -74,34 +76,7 @@ const CategoryHome = ({ addToCart }) => {
       <div className="bg-pink-200 p-4">
         <div className="text-xl font-bold text-center mb-4">Vegetables</div>
         <div className="bg-blue-200 p-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
+          {renderImages(sections.vegetables)}
         </div>
         <div className="bg-pink-200 p-4 text-center mt-4">
           <p className="font-bold">Shop More Vegetables</p>
@@ -112,77 +87,25 @@ const CategoryHome = ({ addToCart }) => {
       <div className="bg-pink-200 p-4">
         <div className="text-xl font-bold text-center mb-4">Spices</div>
         <div className="bg-blue-200 p-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
+          {renderImages(sections.spices)}
         </div>
         <div className="bg-pink-200 p-4 text-center mt-4">
           <p className="font-bold">Shop More Spices</p>
         </div>
       </div>
-            {/* Fruits Section */}
-            <div className="bg-pink-200 p-4">
-        <div className="text-xl font-bold text-center mb-4">Fruits</div>
+
+      {/* Dairy Section */}
+      <div className="bg-pink-200 p-4">
+        <div className="text-xl font-bold text-center mb-4">Dairy</div>
         <div className="bg-blue-200 p-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg"
-              src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-              alt=""
-            />
-          </div>
+          {renderImages(sections.dairy)}
         </div>
         <div className="bg-pink-200 p-4 text-center mt-4">
-          <p className="font-bold">Shop More Fruits</p>
+          <p className="font-bold">Shop More Dairy</p>
         </div>
       </div>
     </div>
   );
 };
+
 export default CategoryHome;
