e497dc212196631d77e56c7d366dbd3530a9b6cb
Diandre Miller <millerdiandre@gmail.com>
2023-09-29 22:39:39 -0400

    fixed recipe component

--- src/Pages/Recipes/CheeseOmeletteRecipe.jsx
+++ src/Pages/Recipes/CheeseOmeletteRecipe.jsx
@@ -2,14 +2,13 @@
 
 import CheeseOmeletteRecipeComponent from "../../NonMVPComponents/CheeseOmeletteRecipeComponent"
 
-const CheeseOmeletteRecipe = ({handleAddToCartClick , addToCart, updateCartWithSpecificIds, cart, setCart }) => {
-    const specificIds = [131, 136, 135, 64]; 
-    console.log("Specific IDs in CheeseOmeletteRecipe:", specificIds);
+const CheeseOmeletteRecipe = ({addToCart}) => {
+    const nameOfIngredients = ["Large White Eggs", "Milk", "Pure Ground Black pepper", "Land OLakes Salted Butter", "Cheese"]
 
 
     return (
         <div>
-            <CheeseOmeletteRecipeComponent handleAddToCartClick={handleAddToCartClick } addToCart={addToCart} setCart={setCart} cart={cart} updateCartWithSpecificIds={updateCartWithSpecificIds} specificIds={specificIds} />
+            <CheeseOmeletteRecipeComponent addToCart={addToCart} nameOfIngredients={nameOfIngredients} />
         </div>
     )
 };
