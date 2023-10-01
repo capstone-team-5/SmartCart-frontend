649d790f31f3418f3a6f55b3fde1f46156c1fb9d
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-01 07:36:50 -0400

    added text below each slide

--- src/MVPComponents/HomeComponent/InternationalSlider.jsx
+++ src/MVPComponents/HomeComponent/InternationalSlider.jsx
@@ -43,32 +43,30 @@ const InternationalSlider = () => {
   const renderSlides = () => {
     return products.map((product, index) => (
       <Slide index={index} key={product.id}>
-        <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-          <img
-            src={product.product_image}
-            alt={product.product_name}
-            className="object-contain object-center w-full h-56"
-          />
-          <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-            {/* <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">
-              {product.product_name}{" "}
-            </h3> */}
-            <div className="flex h-full items-end pb-6">
-              <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">
-                {product.product_name}{" "}
-              </h3>
-            </div>
+        <div className="flex flex-col items-center relative w-full h-full sm:w-auto lg:gap-8 md:gap-6 gap-14 justify-start transition ease-out duration-700">
+          <div className="bg-gray-500 p-4 rounded mb-4">
+            <img
+              src={product.product_image}
+              alt={product.product_name}
+              className="object-contain object-center w-full h-56"
+            />
           </div>
+          <h3 className="text-xl font-semibold leading-5 lg:leading-6 text-black">
+            {product.product_name}
+          </h3>
         </div>
       </Slide>
     ));
   };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex items-center justify-center w-full h-full py-24 sm:py-8 px-4">
+    <div className="mx-auto container p-4 mt-10 bg-orange-200">
+      <h1 className="text-2xl font-semibold p-2 text-black">
+        {" "}
+        Shop our International Categories
+      </h1>
+      <div className="py-24 sm:py-8 px-4">
         <CarouselProvider
-          className="lg:block hidden"
           naturalSlideWidth={100}
           isIntrinsicHeight={true}
           totalSlides={products.length}
@@ -76,22 +74,22 @@ const InternationalSlider = () => {
           step={3}
           infinite={true}
         >
-          <div className="w-full relative flex items-center justify-center">
+          <div className="w-full flex items-center justify-center">
             <ButtonBack
               role="button"
               aria-label="slide backward"
-              className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer"
+              className="absolute z-30 left-0 ml-8 bg-gray-400 ring-2  ring-black focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
               id="prev"
             >
               <GrPrevious size={30} />
             </ButtonBack>
-            <div className="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
-              <Slider>{renderSlides()}</Slider>
-            </div>
+
+            <Slider>{renderSlides()}</Slider>
+
             <ButtonNext
               role="button"
               aria-label="slide forward"
-              className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+              className="absolute z-30 right-0 mr-8 bg-gray-400 ring-2  ring-black focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
               id="next"
             >
               <GrNext size={30} />
@@ -104,74 +102,3 @@ const InternationalSlider = () => {
 };
 
 export default InternationalSlider;
-
-/* Carousel for tablet and medium size devices 
-                <CarouselProvider className="lg:hidden md:block hidden" naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={2} step={1} infinite={true}>
-                    <div className="w-full relative flex items-center justify-center">
-                        <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="prev">
-                            <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M7 1L1 7L7 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </ButtonBack>
-                        <div className="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
-                            <Slider>
-                                <div id="slider" className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
-                                    <Slide index={0}>
-                                        <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                                            <img src="https://i.ibb.co/fDngH9G/carosel-1.png" alt="black chair and white table" className="object-cover object-center w-full" />
-                                            <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Catalog 1</h2>
-                                                <div className="flex h-full items-end pb-6">
-                                                    <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">Minimal Interior</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </Slide>
-                                </div>
-                            </Slider>
-                        </div>
-                        <ButtonNext role="button" aria-label="slide forward" className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400" id="next">
-                            <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 1L7 7L1 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </ButtonNext>
-                    </div>
-                </CarouselProvider>
-
-                {/* Carousel for mobile and Small size Devices 
-                <CarouselProvider className="block md:hidden " naturalSlideWidth={100} isIntrinsicHeight={true} totalSlides={12} visibleSlides={1} step={1} infinite={true}>
-                    <div className="w-full relative flex items-center justify-center">
-                        <ButtonBack role="button" aria-label="slide backward" className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="prev">
-                            <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M7 1L1 7L7 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </ButtonBack>
-                        <div className="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
-                            <Slider>
-                                <div id="slider" className="h-full w-full flex lg:gap-8 md:gap-6 items-center justify-start transition ease-out duration-700">
-                                    <Slide index={0}>
-                                        <div className="flex flex-shrink-0 relative w-full sm:w-auto">
-                                            <img src="https://i.ibb.co/fDngH9G/carosel-1.png" alt="black chair and white table" className="object-cover object-center w-full" />
-                                            <div className="bg-gray-800 bg-opacity-30 absolute w-full h-full p-6">
-                                                <h2 className="lg:text-xl leading-4 text-base lg:leading-5 text-white">Catalog 1</h2>
-                                                <div className="flex h-full items-end pb-6">
-                                                    <h3 className="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white">Minimal Interior</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </Slide>
-                                    
-                                </div>
-                            </Slider>
-                        </div>
-                        <ButtonNext role="button" aria-label="slide forward" className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400" id="next">
-                            <svg width={8} height={14} viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 1L7 7L1 13" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </ButtonNext>
-                    </div>
-                </CarouselProvider>
-            </div>
-        </div>
-    
-*/
