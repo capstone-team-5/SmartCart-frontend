0362577db36b106c0e36fab2ad22bc8e48a78b67
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-08 02:52:16 -0400

    spaced equally

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,5 @@
 import CategoryHome from "../MVPComponents/HomeComponent/CategoryHome";
 import InternationalSlider from "../MVPComponents/HomeComponent/InternationalSlider";
-import Vegetables from "../MVPComponents/HomeComponent/Vegetables";
-import Fruits from "../MVPComponents/HomeComponent/Fruits";
 import Categories from "../MVPComponents/HomeComponent/Categories";
 import NonVegetarianSlider from "../MVPComponents/HomeComponent/NonVegetarianSlider";
 import RecipeSlider from "../MVPComponents/HomeComponent/RecipeSlider";
@@ -13,8 +11,6 @@ const Home = () => {
       <InternationalSlider />
       <NonVegetarianSlider />
       <RecipeSlider />
-      <Fruits />
-      <Vegetables />
     </div>
   );
 };
