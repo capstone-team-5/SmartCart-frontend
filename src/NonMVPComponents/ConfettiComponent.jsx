14f8334bf79dcfe8c4bb5b45a9b83fcfc6886689
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-09 01:50:00 -0400

    confetti

---
+++ src/NonMVPComponents/ConfettiComponent.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
+
+const ConfettiComponent = () => {
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowConfetti(true);
+    }, 300); // 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const confettiWidth = window.innerWidth;
+  const confettiHeight = window.innerHeight;
+
+  return (
+    <div>
+      <Confetti
+        width={confettiWidth}
+        height={confettiHeight}
+        numberOfPieces={showConfetti ? 500 : 0}
+      />
+    </div>
+  );
+};
+
+export default ConfettiComponent;
