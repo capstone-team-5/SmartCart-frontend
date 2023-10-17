da192c751eceff4b5b82676ca3e41c0639f20e5f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-17 08:00:30 -0400

    css updated

--- src/NonMVPComponents/Recipes/Dinner/BakedMacAndCheeseRecipeComponent .jsx
+++ src/NonMVPComponents/Recipes/Dinner/BakedMacAndCheeseRecipeComponent .jsx
@@ -56,49 +56,62 @@ const BakedMacAndCheeseRecipeComponent = ({
   };
 
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <h3 className="text-2xl font-semibold mb-4">Baked Mac and Cheese</h3>
-      <img
-        src="https://www.culinaryhill.com/wp-content/uploads/2022/12/Baked-Mac-and-Cheese-Culinary-Hill-1200x800-warm.jpg"
-        alt="Baked Mac and Cheese"
-        style={{ width: "300px", height: "auto" }}
-      />
-      <div className="recipe">
-        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-        <ul className="list-disc pl-6 mb-4">
-          <li>2 cups elbow macaroni</li>
-          <li>4 cups shredded cheddar cheese</li>
-          <li>1/2 cup grated Parmesan cheese</li>
-          <li>3 cups milk</li>
-          <li>1/4 cup butter</li>
-          <li>2 1/2 teaspoons all-purpose flour</li>
-          <li>2 1/2 teaspoons salt</li>
-          <li>1/2 teaspoon black pepper</li>
-          <li>1/2 teaspoon paprika</li>
-        </ul>
+    <div className="bg-white dark:bg-gray-800 dark:text-white p-4 shadow-md rounded-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <h3 className="text-2xl font-extrabold mb-8 text-center">
+            Baked Mac and Cheese
+          </h3>
+          <img
+            src="https://www.culinaryhill.com/wp-content/uploads/2022/12/Baked-Mac-and-Cheese-Culinary-Hill-1200x800-warm.jpg"
+            alt="Baked Mac and Cheese"
+            className="w-full h-60 object-contain"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-8">Ingredients</h3>
+          <ul className="list-disc pl-6 mb-4">
+            <li className="mb-4">2 cups elbow macaroni</li>
+            <li className="mb-4">4 cups shredded cheddar cheese</li>
+            <li className="mb-4">1/2 cup grated Parmesan cheese</li>
+            <li className="mb-4">3 cups milk</li>
+            <li className="mb-4">1/4 cup butter</li>
+            <li className="mb-4">2 1/2 teaspoons all-purpose flour</li>
+            <li className="mb-4">2 1/2 teaspoons salt</li>
+            <li className="mb-4">1/2 teaspoon black pepper</li>
+            <li className="mb-4">1/2 teaspoon paprika</li>
+          </ul>
+        </div>
+      </div>
 
-        <h3 className="text-lg font-semibold mb-2">Directions</h3>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>
-            <strong>Step 1:</strong> Preheat oven to 350°F (175°C). Cook
-            macaroni according to the package directions until al dente. Drain.
+      <div>
+        <h3 className="text-xl font-bold mb-8">Directions</h3>
+        <ol className="pl-6 mb-4">
+          <li className="mb-4">
+            <strong className="text-xl"> Step 1:</strong> Preheat oven to 350°F
+            (175°C). Cook macaroni according to the package directions until al
+            dente. Drain.
           </li>
-          <li>
-            <strong>Step 2:</strong> In a saucepan, melt butter or margarine
-            over medium heat. Stir in enough flour to make a roux. Add milk to
-            roux slowly, stirring constantly. Stir in cheeses, and cook over low
-            heat until cheese is melted and the sauce is a little thick. Put
-            macaroni in large casserole dish, and pour sauce over macaroni. Stir
-            well.
+          <li className="mb-4">
+            <strong className="text-xl">Step 2:</strong> In a saucepan, melt
+            butter or margarine over medium heat. Stir in enough flour to make a
+            roux. Add milk to roux slowly, stirring constantly.
           </li>
-          <li>
-            <strong>Step 3:</strong> Melt butter or margarine in a skillet over
-            medium heat. Add breadcrumbs and brown. Spread over the macaroni and
-            cheese to cover. Sprinkle with a little paprika.
+          <li className="mb-4">
+            <strong className="text-xl">Step 3:</strong> Stir in cheeses, and
+            cook over low heat until cheese is melted and the sauce is a little
+            thick. Put macaroni in large casserole dish, and pour sauce over
+            macaroni. Stir well.
           </li>
-          <li>
-            <strong>Step 4:</strong> Bake at 350°F (175°C) for 30 minutes.
-            Serve.
+          <li className="mb-4">
+            <strong className="text-xl">Step 4:</strong> Melt butter or
+            margarine in a skillet over medium heat. Add breadcrumbs and brown.
+            Spread over the macaroni and cheese to cover. Sprinkle with a little
+            paprika.
+          </li>
+          <li className="mb-4">
+            <strong className="text-xl">Step 5:</strong> Bake at 350°F (175°C)
+            for 30 minutes. Serve.
           </li>
         </ol>
       </div>
@@ -106,9 +119,9 @@ const BakedMacAndCheeseRecipeComponent = ({
       <div className="flex justify-center">
         <button
           onClick={handleAddToCartClick}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Add Ingredients To Cart
+          Add All Ingredients To Cart
         </button>
       </div>
 
