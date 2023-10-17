3652c83651e616bf77181437c7ef2ae037d44d89
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-17 08:27:53 -0400

    css updated

--- src/NonMVPComponents/Recipes/Dinner/JamaicanJerkChickenRecipeComponent.jsx
+++ src/NonMVPComponents/Recipes/Dinner/JamaicanJerkChickenRecipeComponent.jsx
@@ -56,50 +56,62 @@ const JamaicanJerkChickenRecipeComponent = ({
   };
 
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <h3 className="text-2xl font-semibold mb-4">Jamaican Jerk Chicken</h3>
-      <img
-        src="https://disheswithdad.com/wp-content/uploads/2021/05/jerk-chicken-12.jpg"
-        alt="Jamaican Jerk Chicken"
-        style={{ width: "300px", height: "auto" }}
-      />
-      <div className="recipe">
-        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-        <ul className="list-disc pl-6 mb-4">
-          <li>4 boneless, skinless chicken breasts</li>
-          <li>4 tablespoons Jamaican jerk seasoning</li>
-          <li>2 tablespoons olive oil</li>
-          <li>2 tablespoons soy sauce</li>
-          <li>2 tablespoons lime juice</li>
-          <li>2 cloves garlic, minced</li>
-          <li>1 teaspoon brown sugar</li>
-          <li>1 teaspoon thyme leaves</li>
-          <li>Salt and pepper to taste</li>
-        </ul>
+    <div className="bg-white dark:bg-gray-800 dark:text-white p-4 shadow-md rounded-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <h3 className="text-2xl font-extrabold mb-8 text-center">
+            Jamaican Jerk Chicken
+          </h3>
+          <img
+            src="https://disheswithdad.com/wp-content/uploads/2021/05/jerk-chicken-12.jpg"
+            alt="Jamaican Jerk Chicken"
+            className="w-full h-60 object-contain"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-8">Ingredients</h3>
+          <ul className="list-disc pl-6 mb-4">
+            <li className="mb-4">4 boneless, skinless chicken breasts</li>
+            <li className="mb-4">4 tablespoons Jamaican jerk seasoning</li>
+            <li className="mb-4">2 tablespoons olive oil</li>
+            <li className="mb-4">2 tablespoons soy sauce</li>
+            <li className="mb-4">2 tablespoons lime juice</li>
+            <li className="mb-4">2 cloves garlic, minced</li>
+            <li className="mb-4">1 teaspoon brown sugar</li>
+            <li className="mb-4">1 teaspoon thyme leaves</li>
+            <li className="mb-4">Salt and pepper to taste</li>
+          </ul>
+        </div>
+      </div>
 
-        <h3 className="text-lg font-semibold mb-2">Directions</h3>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>
-            <strong>Step 1:</strong> In a bowl, combine Jamaican jerk seasoning,
-            olive oil, soy sauce, lime juice, minced garlic, brown sugar, thyme
-            leaves, salt, and pepper to make the marinade.
+      <div>
+        <h3 className="text-xl font-bold mb-8">Directions</h3>
+        <ol className="pl-6 mb-4">
+          <li className="mb-4">
+            <strong className="text-xl">Step 1:</strong> In a bowl, combine
+            Jamaican jerk seasoning, olive oil, soy sauce, lime juice, minced
+            garlic, brown sugar, thyme leaves, salt, and pepper to make the
+            marinade.
           </li>
-          <li>
-            <strong>Step 2:</strong> Place chicken breasts in a resealable bag
-            or dish and pour the marinade over them. Seal or cover and marinate
-            in the refrigerator for at least 2 hours or overnight.
+          <li className="mb-4">
+            <strong className="text-xl">Step 2:</strong> Place chicken breasts
+            in a resealable bag or dish and pour the marinade over them. Seal or
+            cover and marinate in the refrigerator for at least 2 hours or
+            overnight.
           </li>
-          <li>
-            <strong>Step 3:</strong> Preheat grill to medium-high heat and oil
-            the grates.
+          <li className="mb-4">
+            <strong className="text-xl">Step 3:</strong> Preheat grill to
+            medium-high heat and oil the grates.
           </li>
-          <li>
-            <strong>Step 4:</strong> Remove chicken from the marinade and grill
-            for 6-8 minutes per side or until fully cooked and charred.
+          <li className="mb-4">
+            <strong className="text-xl">Step 4:</strong> Remove chicken from the
+            marinade and grill for 6-8 minutes per side or until fully cooked
+            and charred.
           </li>
-          <li>
-            <strong>Step 5:</strong> Serve hot with your favorite sides, such as
-            rice and peas, and garnish with lime wedges and fresh cilantro.
+          <li className="mb-4">
+            <strong className="text-xl">Step 5:</strong> Serve hot with your
+            favorite sides, such as rice and peas, and garnish with lime wedges
+            and fresh cilantro.
           </li>
         </ol>
       </div>
@@ -107,9 +119,9 @@ const JamaicanJerkChickenRecipeComponent = ({
       <div className="flex justify-center">
         <button
           onClick={handleAddToCartClick}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Add Ingredients To Cart
+          Add All Ingredients To Cart
         </button>
       </div>
 
