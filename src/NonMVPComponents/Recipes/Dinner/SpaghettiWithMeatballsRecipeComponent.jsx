e521941abe062e2895bc5cec6bfb228a518b3bcb
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-17 08:28:16 -0400

    css updated

--- src/NonMVPComponents/Recipes/Dinner/SpaghettiWithMeatballsRecipeComponent.jsx
+++ src/NonMVPComponents/Recipes/Dinner/SpaghettiWithMeatballsRecipeComponent.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const SpaghettiWithMeatballsRecipeComponent = ({
   addIngredientsToCart,
-  nameOfIngredients
+  nameOfIngredients,
 }) => {
   const [message, setMessage] = useState("");
   const [allItems, setAllItems] = useState([]);
@@ -56,70 +56,78 @@ const SpaghettiWithMeatballsRecipeComponent = ({
   };
 
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <h3 className="text-2xl font-semibold mb-4">Spaghetti with Meatballs</h3>
-      <img
-        src="https://static01.nyt.com/images/2017/04/05/dining/05COOKING-NIGMEATBALLS2/05COOKING-NIGMEATBALLS2-superJumbo.jpg"
-        alt="Spaghetti with Meatballs"
-        style={{ width: "300px", height: "auto" }}
-      />
-      <div className="recipe">
-        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-        <ul className="list-disc pl-6 mb-4">
-          <li>1 pound ground beef</li>
-          <li>1/2 pound ground pork</li>
-          <li>1/2 cup breadcrumbs</li>
-          <li>1/4 cup grated Parmesan cheese</li>
-          <li>1/4 cup milk</li>
-          <li>1/4 cup fresh parsley, chopped</li>
-          <li>1 egg</li>
-          <li>1 teaspoon salt</li>
-          <li>1/2 teaspoon black pepper</li>
-          <li>1/2 teaspoon garlic powder</li>
-          <li>1/2 teaspoon onion powder</li>
-          <li>1/4 cup vegetable oil</li>
-          <li>1 can (24 oz) tomato sauce</li>
-          <li>1 can (14 oz) crushed tomatoes</li>
-          <li>1 teaspoon dried basil</li>
-          <li>1 teaspoon dried oregano</li>
-          <li>1/2 teaspoon sugar</li>
-          <li>1 pound spaghetti</li>
-        </ul>
+    <div className="bg-white dark:bg-gray-800 dark:text-white p-4 shadow-md rounded-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <h3 className="text-2xl font-extrabold mb-8 text-center">
+            Spaghetti with Meatballs
+          </h3>
+          <img
+            src="https://static01.nyt.com/images/2017/04/05/dining/05COOKING-NIGMEATBALLS2/05COOKING-NIGMEATBALLS2-superJumbo.jpg"
+            alt="Spaghetti with Meatballs"
+            className="w-full h-60 object-contain"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-8">Ingredients</h3>
+          <ul className="list-disc pl-6 mb-4">
+            <li className="mb-4">1 pound ground beef</li>
+            <li className="mb-4">1/2 pound ground pork</li>
+            <li className="mb-4">1/2 cup breadcrumbs</li>
+            <li className="mb-4">1/4 cup grated Parmesan cheese</li>
+            <li className="mb-4">1/4 cup milk</li>
+            <li className="mb-4">1/4 cup fresh parsley, chopped</li>
+            <li className="mb-4">1 egg</li>
+            <li className="mb-4">1 teaspoon salt</li>
+            <li className="mb-4">1/2 teaspoon black pepper</li>
+            <li className="mb-4">1/2 teaspoon garlic powder</li>
+            <li className="mb-4">1/2 teaspoon onion powder</li>
+            <li className="mb-4">1/4 cup vegetable oil</li>
+            <li className="mb-4">1 can (24 oz) tomato sauce</li>
+            <li className="mb-4">1 can (14 oz) crushed tomatoes</li>
+            <li className="mb-4">1 teaspoon dried basil</li>
+            <li className="mb-4">1 teaspoon dried oregano</li>
+            <li className="mb-4">1/2 teaspoon sugar</li>
+            <li className="mb-4">1 pound spaghetti</li>
+          </ul>
+        </div>
+      </div>
 
-        <h3 className="text-lg font-semibold mb-2">Directions</h3>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>
-            <strong>Step 1:</strong> In a large bowl, combine ground beef,
-            ground pork, breadcrumbs, Parmesan cheese, milk, chopped parsley,
-            egg, salt, black pepper, garlic powder, and onion powder. Mix until
-            well combined.
+      <div>
+        <h3 className="text-xl font-bold mb-8">Directions</h3>
+        <ol className="pl-6 mb-4">
+          <li className="mb-4">
+            <strong className="text-xl">Step 1:</strong> In a large bowl,
+            combine ground beef, ground pork, breadcrumbs, Parmesan cheese,
+            milk, chopped parsley, egg, salt, black pepper, garlic powder, and
+            onion powder. Mix until well combined.
           </li>
-          <li>
-            <strong>Step 2:</strong> Form the mixture into meatballs, about 1
-            inch in diameter.
+          <li className="mb-4">
+            <strong className="text-xl">Step 2:</strong> Form the mixture into
+            meatballs, about 1 inch in diameter.
           </li>
-          <li>
-            <strong>Step 3:</strong> Heat vegetable oil in a large skillet over
-            medium heat. Add meatballs and brown on all sides. Remove and drain
-            on paper towels.
+          <li className="mb-4">
+            <strong className="text-xl">Step 3:</strong> Heat vegetable oil in a
+            large skillet over medium heat. Add meatballs and brown on all
+            sides. Remove and drain on paper towels.
           </li>
-          <li>
-            <strong>Step 4:</strong> In the same skillet, add tomato sauce,
-            crushed tomatoes, dried basil, dried oregano, and sugar. Stir to
-            combine. Return the meatballs to the skillet.
+          <li className="mb-4">
+            <strong className="text-xl">Step 4:</strong> In the same skillet,
+            add tomato sauce, crushed tomatoes, dried basil, dried oregano, and
+            sugar. Stir to combine. Return the meatballs to the skillet.
           </li>
-          <li>
-            <strong>Step 5:</strong> Simmer the sauce and meatballs for 20-30
-            minutes, or until the meatballs are cooked through and the sauce is
-            heated.
+          <li className="mb-4">
+            <strong className="text-xl">Step 5:</strong> Simmer the sauce and
+            meatballs for 20-30 minutes, or until the meatballs are cooked
+            through and the sauce is heated.
           </li>
-          <li>
-            <strong>Step 6:</strong> Cook spaghetti according to package
-            instructions. Drain.
+          <li className="mb-4">
+            <strong className="text-xl">Step 6:</strong> Cook spaghetti
+            according to package instructions. Drain.
           </li>
-          <li>
-            <strong>Step 7:</strong> Serve meatballs and sauce over cooked
-            spaghetti.
+          <li className="mb-4">
+            <strong className="text-xl">Step 7:</strong> Serve meatballs and
+            sauce over cooked spaghetti.
           </li>
         </ol>
       </div>
@@ -127,9 +135,9 @@ const SpaghettiWithMeatballsRecipeComponent = ({
       <div className="flex justify-center">
         <button
           onClick={handleAddToCartClick}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Add Ingredients To Cart
+          Add All Ingredients To Cart
         </button>
       </div>
 
