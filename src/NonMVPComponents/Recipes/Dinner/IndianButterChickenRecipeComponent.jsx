6d209b1a86ba37fc8d71e54c49c095711277ca78
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-17 08:08:03 -0400

    css updated

--- src/NonMVPComponents/Recipes/Dinner/IndianButterChickenRecipeComponent.jsx
+++ src/NonMVPComponents/Recipes/Dinner/IndianButterChickenRecipeComponent.jsx
@@ -56,60 +56,68 @@ const IndianButterChickenRecipeComponent = ({
   };
 
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <h3 className="text-2xl font-semibold mb-4">Indian Butter Chicken</h3>
-      <img
-        src="https://www.thecookierookie.com/wp-content/uploads/2022/08/Featured-Indian-butter-chicken-1.jpg"
-        alt="Indian Butter Chicken"
-        style={{ width: "300px", height: "auto" }}
-      />
-      <div className="recipe">
-        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-        <ul className="list-disc pl-6 mb-4">
-          <li>
-            1.5 lbs boneless, skinless chicken thighs, cut into bite-sized
-            pieces
-          </li>
-          <li>1 cup plain yogurt</li>
-          <li>2 tablespoons lemon juice</li>
-          <li>2 teaspoons ground turmeric</li>
-          <li>2 teaspoons garam masala</li>
-          <li>2 teaspoons ground cumin</li>
-          <li>2 teaspoons chili powder</li>
-          <li>2 teaspoons paprika</li>
-          <li>1 teaspoon ground cinnamon</li>
-          <li>1 teaspoon ground coriander</li>
-          <li>4 cloves garlic, minced</li>
-          <li>2 tablespoons grated fresh ginger</li>
-          <li>1 cup tomato puree</li>
-          <li>1 cup heavy cream</li>
-          <li>1/4 cup unsalted butter</li>
-          <li>Salt and pepper to taste</li>
-          <li>Chopped fresh cilantro for garnish</li>
-        </ul>
+    <div className="bg-white dark:bg-gray-800 dark:text-white p-4 shadow-md rounded-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <h3 className="text-2xl font-extrabold mb-8 text-center">
+            Indian Butter Chicken
+          </h3>
+          <img
+            src="https://www.thecookierookie.com/wp-content/uploads/2022/08/Featured-Indian-butter-chicken-1.jpg"
+            alt="Indian Butter Chicken"
+            className="w-full h-60 object-contain"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-8">Ingredients</h3>
+          <ul className="list-disc pl-6 mb-4">
+            <li className="mb-4">
+              1.5 lbs boneless, skinless chicken thighs, cut into bite-sized
+              pieces
+            </li>
+            <li className="mb-4">1 cup plain yogurt</li>
+            <li className="mb-4">2 tablespoons lemon juice</li>
+            <li className="mb-4">2 teaspoons ground turmeric</li>
+            <li className="mb-4">2 teaspoons garam masala</li>
+            <li className="mb-4">2 teaspoons ground cumin</li>
+            <li className="mb-4">2 teaspoons chili powder</li>
+            <li className="mb-4">2 teaspoons paprika</li>
+            <li className="mb-4">1 teaspoon ground cinnamon</li>
+            <li className="mb-4">1 teaspoon ground coriander</li>
+            <li className="mb-4">4 cloves garlic, minced</li>
+            <li className="mb-4">2 tablespoons grated fresh ginger</li>
+            <li className="mb-4">1 cup tomato puree</li>
+            <li className="mb-4">1 cup heavy cream</li>
+            <li className="mb-4">1/4 cup unsalted butter</li>
+            <li className="mb-4">Salt and pepper to taste</li>
+            <li className="mb-4">Chopped fresh cilantro for garnish</li>
+          </ul>
+        </div>
+      </div>
 
-        <h3 className="text-lg font-semibold mb-2">Directions</h3>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>
-            <strong>Step 1:</strong> In a large bowl, combine yogurt, lemon
-            juice, turmeric, garam masala, cumin, chili powder, paprika,
-            cinnamon, coriander, garlic, and ginger. Mix well. Add chicken
-            pieces and marinate for at least 1 hour, preferably overnight in the
-            refrigerator.
+      <div>
+        <h3 className="text-xl font-bold mb-8">Directions</h3>
+        <ol className="pl-6 mb-4">
+          <li className="mb-4">
+            <strong className="text-xl">Step 1:</strong> In a large bowl,
+            combine yogurt, lemon juice, turmeric, garam masala, cumin, chili
+            powder, paprika, cinnamon, coriander, garlic, and ginger. Mix well.
+            Add chicken pieces and marinate for at least 1 hour, preferably
+            overnight in the refrigerator.
           </li>
-          <li>
-            <strong>Step 2:</strong> Heat butter in a large skillet over medium
-            heat. Add marinated chicken and cook until browned and cooked
-            through, about 10 minutes.
+          <li className="mb-4">
+            <strong className="text-xl">Step 2:</strong> Heat butter in a large
+            skillet over medium heat. Add marinated chicken and cook until
+            browned and cooked through, about 10 minutes.
           </li>
-          <li>
-            <strong>Step 3:</strong> Stir in tomato puree and simmer for 10
-            minutes. Add cream and simmer for an additional 10 minutes. Season
-            with salt and pepper to taste.
+          <li className="mb-4">
+            <strong className="text-xl">Step 3:</strong> Stir in tomato puree
+            and simmer for 10 minutes. Add cream and simmer for an additional 10
+            minutes. Season with salt and pepper to taste.
           </li>
-          <li>
-            <strong>Step 4:</strong> Serve hot, garnished with chopped fresh
-            cilantro. Serve with rice or naan bread.
+          <li className="mb-4">
+            <strong className="text-xl">Step 4:</strong> Serve hot, garnished
+            with chopped fresh cilantro. Serve with rice or naan bread.
           </li>
         </ol>
       </div>
@@ -117,9 +125,9 @@ const IndianButterChickenRecipeComponent = ({
       <div className="flex justify-center">
         <button
           onClick={handleAddToCartClick}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Add Ingredients To Cart
+          Add All Ingredients To Cart
         </button>
       </div>
 
