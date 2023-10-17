68cc8783adde87cffaa96893ec32f7efda9c1359
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-10-17 08:28:36 -0400

    css updated

--- src/NonMVPComponents/Recipes/Dinner/SpanishRiceAndBeansRecipeComponent.jsx
+++ src/NonMVPComponents/Recipes/Dinner/SpanishRiceAndBeansRecipeComponent.jsx
@@ -56,59 +56,73 @@ const SpanishRiceAndBeansRecipeComponent = ({
   };
 
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <h3 className="text-2xl font-semibold mb-4">Spanish Rice and Beans</h3>
-      <img
-        src="https://www.liveeatlearn.com/wp-content/uploads/2019/02/spanish-rice-beans-vert-500x375.jpg"
-        alt="Spanish Rice and Beans"
-        style={{ width: "300px", height: "auto" }}
-      />
-      <div className="recipe">
-        <h3 className="text-lg font-semibold mb-2">Ingredients</h3>
-        <ul className="list-disc pl-6 mb-4">
-          <li>1 cup long-grain white rice</li>
-          <li>2 cups chicken or vegetable broth</li>
-          <li>1 can (15 oz) black beans, drained and rinsed</li>
-          <li>1 can (15 oz) diced tomatoes with green chilies</li>
-          <li>1 small onion, diced</li>
-          <li>1 bell pepper, diced</li>
-          <li>2 cloves garlic, minced</li>
-          <li>2 teaspoons ground cumin</li>
-          <li>1 teaspoon chili powder</li>
-          <li>1/2 teaspoon paprika</li>
-          <li>Salt and pepper to taste</li>
-          <li>Chopped fresh cilantro for garnish</li>
-        </ul>
+    <div className="bg-white dark:bg-gray-800 dark:text-white p-4 shadow-md rounded-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <h3 className="text-2xl font-extrabold mb-8 text-center">
+            Spanish Rice and Beans
+          </h3>
+          <img
+            src="https://www.liveeatlearn.com/wp-content/uploads/2019/02/spanish-rice-beans-vert-500x375.jpg"
+            alt="Spanish Rice and Beans"
+            className="w-full h-60 object-contain"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-8">Ingredients</h3>
+          <ul className="list-disc pl-6 mb-4">
+            <li className="mb-4">1 cup long-grain white rice</li>
+            <li className="mb-4">2 cups chicken or vegetable broth</li>
+            <li className="mb-4">
+              1 can (15 oz) black beans, drained and rinsed
+            </li>
+            <li className="mb-4">
+              1 can (15 oz) diced tomatoes with green chilies
+            </li>
+            <li className="mb-4">1 small onion, diced</li>
+            <li className="mb-4">1 bell pepper, diced</li>
+            <li className="mb-4">2 cloves garlic, minced</li>
+            <li className="mb-4">2 teaspoons ground cumin</li>
+            <li className="mb-4">1 teaspoon chili powder</li>
+            <li className="mb-4">1/2 teaspoon paprika</li>
+            <li className="mb-4">Salt and pepper to taste</li>
+            <li className="mb-4">Chopped fresh cilantro for garnish</li>
+          </ul>
+        </div>
+      </div>
 
-        <h3 className="text-lg font-semibold mb-2">Directions</h3>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>
-            <strong>Step 1:</strong> In a large skillet, heat a bit of oil over
-            medium heat. Add diced onions and bell peppers and sauté until
-            softened, about 3-4 minutes.
+      <div>
+        <h3 className="text-xl font-bold mb-8">Directions</h3>
+        <ol className="pl-6 mb-4">
+          <li className="mb-4">
+            <strong className="text-xl">Step 1:</strong> In a large skillet,
+            heat a bit of oil over medium heat. Add diced onions and bell
+            peppers and sauté until softened, about 3-4 minutes.
           </li>
-          <li>
-            <strong>Step 2:</strong> Add minced garlic and cook for an
-            additional 30 seconds until fragrant.
+          <li className="mb-4">
+            <strong className="text-xl">Step 2:</strong> Add minced garlic and
+            cook for an additional 30 seconds until fragrant.
           </li>
-          <li>
-            <strong>Step 3:</strong> Add rice to the skillet and cook for 2-3
-            minutes, stirring occasionally until the rice is lightly toasted.
+          <li className="mb-4">
+            <strong className="text-xl">Step 3:</strong> Add rice to the skillet
+            and cook for 2-3 minutes, stirring occasionally until the rice is
+            lightly toasted.
           </li>
-          <li>
-            <strong>Step 4:</strong> Stir in chicken or vegetable broth, diced
-            tomatoes with green chilies, black beans, ground cumin, chili
-            powder, paprika, salt, and pepper. Bring to a boil.
+          <li className="mb-4">
+            <strong className="text-xl">Step 4:</strong> Stir in chicken or
+            vegetable broth, diced tomatoes with green chilies, black beans,
+            ground cumin, chili powder, paprika, salt, and pepper. Bring to a
+            boil.
           </li>
-          <li>
-            <strong>Step 5:</strong> Reduce heat to low, cover, and simmer for
-            20-25 minutes or until the rice is cooked and the liquid is
-            absorbed.
+          <li className="mb-4">
+            <strong className="text-xl">Step 5:</strong> Reduce heat to low,
+            cover, and simmer for 20-25 minutes or until the rice is cooked and
+            the liquid is absorbed.
           </li>
-          <li>
-            <strong>Step 6:</strong> Remove from heat and let it sit for 5
-            minutes. Fluff with a fork and garnish with chopped fresh cilantro
-            before serving.
+          <li className="mb-4">
+            <strong className="text-xl">Step 6:</strong> Remove from heat and
+            let it sit for 5 minutes. Fluff with a fork and garnish with chopped
+            fresh cilantro before serving.
           </li>
         </ol>
       </div>
@@ -116,9 +130,9 @@ const SpanishRiceAndBeansRecipeComponent = ({
       <div className="flex justify-center">
         <button
           onClick={handleAddToCartClick}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg"
+          className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
         >
-          Add Ingredients To Cart
+          Add All Ingredients To Cart
         </button>
       </div>
 
